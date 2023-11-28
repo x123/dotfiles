1a827142cecf367e2e98d71d1a03231683af8423
x123 <x123@users.noreply.github.com>
2023-11-28 22:22:51 +0100

    remove system gnupg agent

--- system/xnix/configuration.nix
+++ system/xnix/configuration.nix
@@ -78,10 +78,6 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
 
   programs.steam = {
     enable = true;
