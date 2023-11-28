938789058ee12127098ff36243f5bc54c15e6d92
x123 <x123@users.noreply.github.com>
2023-11-28 22:06:24 +0100

    modular alacritty

---
+++ users/modules/alacritty.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  imports = [];
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 10;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+}
