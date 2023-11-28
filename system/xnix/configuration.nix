7131130816a59258a1bcfe9a4ef0f297e1dd2208
x123 <x123@users.noreply.github.com>
2023-11-28 21:46:38 +0100

    cruft removal

--- system/xnix/configuration.nix
+++ system/xnix/configuration.nix
@@ -53,13 +53,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.x = {
     isNormalUser = true;
