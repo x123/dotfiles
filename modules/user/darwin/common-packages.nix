3ac2c21c93cc16726b922f4c46220e909732ae0b
x123 <x123@users.noreply.github.com>
2023-11-29 19:25:37 +0100

    add darwin common packages

---
+++ modules/user/darwin/common-packages.nix
@@ -0,0 +1,42 @@
+{pkgs, ...}: {
+  imports = [];
+
+  home = {
+    packages = with pkgs; [
+      # term/shell
+      file
+      htop
+      pciutils
+      ripgrep
+      tmux
+      whois
+
+      # net
+      aria2
+
+      # dev
+      git
+      git-crypt
+      jq
+      yq
+
+      # crypto
+      age
+      gnupg
+      sops
+
+      # archives
+      unzip
+      zip
+
+      # network tools
+      dnsutils
+      ipcalc
+      mtr
+      nmap
+
+      # misc
+      pinentry
+    ];
+  };
+}
