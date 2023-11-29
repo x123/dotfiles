37bdfcc65be556c42b3f58265dea88408061f32f
x123 <x123@users.noreply.github.com>
2023-11-29 08:32:00 +0100

    starship update window title

--- users/modules/shell.nix
+++ users/modules/shell.nix
@@ -6,6 +6,11 @@
     enableCompletion = true;
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '' + ''
+     function set_win_title(){
+       echo -ne "\033]0; $(basename "$PWD") \007"
+     }
+     starship_precmd_user_func="set_win_title"
     '';
     shellAliases = {
       adamantium = "ssh adamantium";
