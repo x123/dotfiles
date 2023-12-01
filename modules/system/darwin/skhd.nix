9c674e52d31311f4ad87640d5437c1b8e528e763
x123 <x123@users.noreply.github.com>
2023-12-01 10:16:44 +0100

    yabai/skhd bindings

--- modules/system/darwin/skhd.nix
+++ modules/system/darwin/skhd.nix
@@ -4,26 +4,28 @@
   services.skhd = {
     enable = true;
     skhdConfig = ''
-	# open terminal
-	alt - return : /Users/fom/Applications/Home\ Manager\ Apps/Alacritty.app/Contents/MacOS/alacritty
+    # open terminal
+    alt - return : /Users/fom/Applications/Home\ Manager\ Apps/Alacritty.app/Contents/MacOS/alacritty
 
-	# focus window
-	alt - left : yabai -m window --focus west
-	alt - down : yabai -m window --focus south
-	alt - up : yabai -m window --focus north
-	alt - right : yabai -m window --focus east
+    # focus window
+    alt - left : yabai -m window --focus west
+    alt - down : yabai -m window --focus south
+    alt - up : yabai -m window --focus north
+    alt - right : yabai -m window --focus east
 
-	# swap window
-	shift + alt - left : yabai -m window --swap west
-	shift + alt - down : yabai -m window --swap south
-	shift + alt - up : yabai -m window --swap north
-	shift + alt - right : yabai -m window --swap east
+    # swap window
+    shift + cmd - left : yabai -m window --swap west
+    shift + cmd - down : yabai -m window --swap south
+    shift + cmd - up : yabai -m window --swap north
+    shift + cmd - right : yabai -m window --swap east
 
-	# move window
-	shift + cmd - left : yabai -m window --warp west
-	shift + cmd - down : yabai -m window --warp south
-	shift + cmd - up : yabai -m window --warp north
-    shift + cmd - right : yabai -m window --warp east
+    # move window
+    shift + alt - left : yabai -m window --warp west
+    shift + alt - down : yabai -m window --warp south
+    shift + alt - up : yabai -m window --warp north
+    shift + alt - right : yabai -m window --warp east
+
+    shift + alt - c : launchctl kickstart -k "gui/''${UID}/org.nixos.yabai" && launchctl kickstart -k "gui/''${UID}/org.nixos.skhd"
 
     # float / unfloat window and center on screen
     alt - t : yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2
