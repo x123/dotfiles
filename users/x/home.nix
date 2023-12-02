f375cd1b1542856c27fb3ec60c5ffc4629b87b02
x123 <x123@users.noreply.github.com>
2023-12-02 12:27:51 +0100

    add pytorch

--- users/x/home.nix
+++ users/x/home.nix
@@ -10,6 +10,7 @@
     ../../modules/user/git.nix
     ../../modules/user/gpg-agent.nix
     ../../modules/user/keepass.nix
+    ../../modules/user/pytorch.nix
     ../../modules/user/shell
     ../../modules/user/tor-browser.nix
     ../../modules/user/video.nix
