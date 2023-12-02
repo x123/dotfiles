356ba9c6fba3e128f5decc45d42b87e9761a9bb9
x123 <x123@users.noreply.github.com>
2023-12-02 15:01:37 +0100

    add ai cache

--- system/xnix/configuration.nix
+++ system/xnix/configuration.nix
@@ -17,6 +17,9 @@
     ../../modules/system/zsh.nix
     ];
 
+  nix.settings.trusted-substituters = ["https://ai.cachix.org"];
+  nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
+
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
