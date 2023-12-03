568519930ef65744d4ea48a7a6df3c1434962b35
x123 <x123@users.noreply.github.com>
2023-12-03 13:38:36 +0100

    add nord theme to Xresources

--- modules/user/x11/default.nix
+++ modules/user/x11/default.nix
@@ -3,6 +3,15 @@
     ./i3-config.nix
   ];
 
+  xresources.extraConfig = builtins.readFile (
+    pkgs.fetchFromGitHub {
+      owner = "nordtheme";
+      repo = "xresources";
+      rev = "ba3b1b61bf6314abad4055eacef2f7cbea1924fb";
+      sha256 = "vw0lD2XLKhPS1zElNkVOb3zP/Kb4m0VVgOakwoJxj74=";
+    } + "/src/nord"
+  );
+
   home = {
     packages = with pkgs; [
       # term/shell
