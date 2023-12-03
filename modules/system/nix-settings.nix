c6d13e0be9ae6462f7e789fe6c7c18a01584aad3
x123 <x123@users.noreply.github.com>
2023-12-03 18:53:47 +0100

    enable auto gc

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -5,4 +5,15 @@
   nix.package = pkgs.nixFlakes;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.gc = (if pkgs.stdenv.isDarwin then {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 0; Minute = 0;};
+    options = "--delete-older-than 30d";
+  }
+  else
+  {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  });
 }
