7846099c182f9409424da5d79001285ff3860dd2
x123 <x123@users.noreply.github.com>
2023-12-06 09:03:39 +0000

    disable karabiner

--- system/fom-mba14/configuration.nix
+++ system/fom-mba14/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports = [
   ../../modules/system/darwin/fonts.nix
-  ../../modules/system/darwin/karabiner.nix
+  #../../modules/system/darwin/karabiner.nix
   ../../modules/system/nix-settings.nix # do not remove
   #../../modules/system/darwin/yabai.nix
   ../../modules/system/darwin/skhd.nix
