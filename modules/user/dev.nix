cd72fffb849b76a5e6b1d2eca5993bb23ada24f3
x123 <x123@users.noreply.github.com>
2023-12-25 18:20:27 +0100

    switch to direnv

--- modules/user/dev.nix
+++ modules/user/dev.nix
@@ -1,11 +1,18 @@
 {pkgs, ...}: {
   imports = [];
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
   home = {
     packages = with pkgs; [
-      elixir
-      erlang
-      nodejs
+      #elixir
+      #erlang
+      #nodejs
       sqlite
     ];
   };
