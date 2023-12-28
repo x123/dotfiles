503f820b965a25a4b747747a8bc94925e1c63e25
x123 <x123@users.noreply.github.com>
2023-12-28 12:07:22 +0100

    configure git-delta

--- modules/user/git.nix
+++ modules/user/git.nix
@@ -5,6 +5,19 @@
     enable = true;
     userName = "x123";
     userEmail = "x123@users.noreply.github.com";
+
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        light = false;
+        conflictStyle = "diff3";
+        colorMoved = "default";
+        syntax-theme = "zenburn";
+        side-by-side = false;
+      };
+    };
+
     extraConfig = {
       init.defaultBranch = "master";
       push.autoSetupRemote = true;
