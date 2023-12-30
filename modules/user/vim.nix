9059409382e9ddeaab97b4c95dee2b0bf764e6fc
x123 <x123@users.noreply.github.com>
2023-12-30 22:37:54 +0100

    add reasonable neovim

--- modules/user/vim.nix
+++ modules/user/vim.nix
@@ -1,6 +1,91 @@
 {pkgs, ...}: {
   imports = [];
 
+  home = {
+    packages = with pkgs; [
+      elixir-ls
+      gopls
+      marksman
+      nerdfonts
+      nixd
+      terraform-ls
+    ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs; [
+      vimPlugins.lualine-nvim
+      vimPlugins.mason-lspconfig-nvim
+      vimPlugins.mason-nvim
+      vimPlugins.nord-nvim
+      vimPlugins.nvim-lastplace
+      vimPlugins.nvim-lspconfig
+      vimPlugins.nvim-treesitter
+      vimPlugins.nvim-treesitter-parsers.markdown
+      vimPlugins.nvim-treesitter-parsers.go
+      vimPlugins.nvim-treesitter-parsers.elixir
+      vimPlugins.nvim-treesitter-parsers.json
+      vimPlugins.nvim-treesitter-parsers.nix
+      vimPlugins.nvim-treesitter-parsers.terraform
+      vimPlugins.tmux-nvim
+    ];
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    extraConfig = ''
+      syntax on
+      filetype plugin indent on
+      set cursorline
+      set showmatch
+      let g:airline_theme='base16_nord'
+      set number relativenumber
+      set expandtab
+      set tabstop=2
+      set shiftwidth=2
+      set background=dark
+
+      nnoremap <C-up> <cmd>lua require("tmux").move_top()<cr>
+      nnoremap <C-down> <cmd>lua require("tmux").move_bottom()<cr>
+      nnoremap <C-left> <cmd>lua require("tmux").move_left()<cr>
+      nnoremap <C-right> <cmd>lua require("tmux").move_right()<cr>
+    '';
+    extraLuaConfig = ''
+      vim.opt.termguicolors = true
+      require('lualine').setup{
+        options = {
+          theme = 'nord'
+        }
+      }
+
+      require("tmux").setup{
+        options = {
+          navigation = {
+            enable_default_keybindings = false,
+          },
+          resize = {
+            enable_default_keybindings = false,
+          }
+        }
+      }
+
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.elixirls.setup {
+      	cmd = { "${pkgs.elixir-ls}/bin/elixir-ls" };
+      }
+      lspconfig.gopls.setup {}
+      lspconfig.lua_ls.setup {}
+      lspconfig.marksman.setup {}
+      lspconfig.nixd.setup {}
+      lspconfig.terraformls.setup {}
+
+      vim.cmd[[colorscheme nord]]
+    '';
+  };
+
   programs.vim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
@@ -16,7 +101,7 @@
       vim-nix
       vim-tmux-navigator
     ];
-    defaultEditor = true;
+    defaultEditor = false;
     settings = {
       number = true;
       relativenumber = true;
