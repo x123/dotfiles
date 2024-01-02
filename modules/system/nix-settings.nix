4f749432fce61b03dc2b19ee432f2ce5cff25777
x123 <x123@users.noreply.github.com>
2024-01-02 06:46:31 +0100

    nix auto-optimise & gc interval

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -8,12 +8,14 @@
   nix.gc = (if pkgs.stdenv.isDarwin then {
     automatic = true;
     interval = { Weekday = 0; Hour = 0; Minute = 0;};
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 7d";
   }
   else
   {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 7d";
   });
+
+  nix.settings.auto-optimise-store = true;
 }
