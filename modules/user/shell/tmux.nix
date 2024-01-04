1d6552cca45630279a36fd63d1efac90e55c442a
x123 <x123@users.noreply.github.com>
2024-01-04 21:50:08 +0100

    set tmux term to screen-256color

--- modules/user/shell/tmux.nix
+++ modules/user/shell/tmux.nix
@@ -13,6 +13,7 @@
     extraConfig = ''
       # Set true color
       set-option -sa terminal-overrides ",xterm*:Tc"
+      set-option -g default-terminal "screen-256color"
 
       set-option -g bell-action none
       set-option -g visual-bell off
