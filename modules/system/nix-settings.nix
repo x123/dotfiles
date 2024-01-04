6495c9c0a0370837b2e2963368bd419bb5f1355a
x123 <x123@users.noreply.github.com>
2024-01-04 13:07:04 +0100

    trusted substituters and cleanup time

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -5,16 +5,19 @@
   nix.package = pkgs.nixFlakes;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.settings.trusted-substituters = ["https://nixium.boxchop.city"];
+  nix.settings.trusted-public-keys = ["nixium.boxchop.city:VqGEePxRjPwhVfnLAJBi2duwwkIczIy5ODGW/8KCPbc"];
+
   nix.gc = (if pkgs.stdenv.isDarwin then {
     automatic = true;
     interval = { Weekday = 0; Hour = 0; Minute = 0;};
-    options = "--delete-older-than 7d";
+    options = "--delete-older-than 30d";
   }
   else
   {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 7d";
+    options = "--delete-older-than 30d";
   });
 
   nix.settings.auto-optimise-store = true;
