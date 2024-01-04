8bbb86fe6f843a7ac6feb010aaa3839d5d5a9482
x123 <x123@users.noreply.github.com>
2024-01-04 22:02:13 +0100

    fix mosh color issue

--- modules/user/neovim.nix
+++ modules/user/neovim.nix
@@ -156,6 +156,12 @@
         end,
       })
 
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = false
+      vim.g.nord_italic = false
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = true
       vim.cmd[[colorscheme nord]]
     '';
   };
