95d87473625d58987e34ee6c2dbaff5b625fc587
x123 <x123@users.noreply.github.com>
2024-01-04 15:24:34 +0100

    cleanup nix settings format

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -5,18 +5,27 @@
   nix.package = pkgs.nixFlakes;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  nix.settings.trusted-substituters = ["https://nixium.boxchop.city"];
-  nix.settings.trusted-public-keys = ["nixium.boxchop.city:VqGEePxRjPwhVfnLAJBi2duwwkIczIy5ODGW/8KCPbc"];
-  nix.settings.auto-optimise-store = true;
-  nix.settings.allowed-users = [
-    "*"
-  ];
-  nix.settings.trusted-users = [
-    "root"
-    "x"
-    "fom"
-    "@wheel"
-  ];
+  nix.settings = {
+    trusted-substituters = [
+      "https://nixium.boxchop.city"
+    ];
+
+    trusted-public-keys = [
+      "nixium.boxchop.city:VqGEePxRjPwhVfnLAJBi2duwwkIczIy5ODGW/8KCPbc"
+    ];
+
+    auto-optimise-store = true;
+    allowed-users = [
+      "*"
+    ];
+    trusted-users = [
+      "root"
+      "x"
+      "fom"
+      "@wheel"
+    ];
+  };
+
   nix.gc = (if pkgs.stdenv.isDarwin then {
     automatic = true;
     interval = { Weekday = 0; Hour = 0; Minute = 0;};
