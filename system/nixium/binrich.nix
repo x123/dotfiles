5983f40fe1e6e3fec3276cc33ca43b6c21614739
x123 <x123@users.noreply.github.com>
2024-01-22 18:12:33 +0100

    add binrich user

--- system/nixium/binrich.nix
+++ system/nixium/binrich.nix
@@ -1,5 +1,21 @@
 { config, pkgs, ... }: {
   imports = [ ];
 
-  sops.secrets."postgres/nixium/binrichfile" = { };
+  sops.secrets."postgres/nixium/binrichfile" = {
+    mode = "0400";
+    owner = config.users.users.binrich.name;
+  };
+
+  users.users.binrich = {
+    createHome = true;
+    isNormalUser = true;
+    description = "binrich";
+    shell = pkgs.zsh;
+    useDefaultShell = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAV4W4TVF5yqOwKFax+b2XtRYbdKy1wy4zFXfFZfv5be xnix"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6elYl8CWSR32Zx33D+XgQWM/721sDmnyFJec7vDeMb fom-mba"
+    ];
+  };
+
 }
