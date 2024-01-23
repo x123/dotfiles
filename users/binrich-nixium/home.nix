452530aecb3ce1709bc5c0dd8a5ec99c525a6154
x123 <x123@users.noreply.github.com>
2024-01-23 11:28:40 +0100

    initial systemd binrich

--- users/binrich-nixium/home.nix
+++ users/binrich-nixium/home.nix
@@ -2,27 +2,14 @@
 
 {
   imports = [
-    #<sops-nix/modules/home-manager/sops.nix>
-    #../../modules/user/ai.nix
-    #../../modules/user/alacritty
-    #../../modules/user/blender.nix
-    #../../modules/user/calibre.nix
-    #../../modules/user/chromium.nix
     ../../modules/user/binrich.nix
     ../../modules/user/common-packages.nix
     ../../modules/user/common-ssh.nix
     ../../modules/user/dev.nix
-    #../../modules/user/discord.nix
-    #../../modules/user/firefox.nix
     ../../modules/user/git.nix
-    #../../modules/user/gpg-agent.nix
-    #../../modules/user/keepass.nix
-    #../../modules/user/pytorch.nix
-    ../../modules/user/shell
-    #../../modules/user/tor-browser.nix
-    #../../modules/user/video.nix
     ../../modules/user/neovim.nix
-    #../../modules/user/x11
+    ../../modules/user/shell
+    ../../modules/user/systemd/service-binrich.nix
   ];
 
   home = {
