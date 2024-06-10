3428c1dd64fa7f41fd00702e8603f6d6507a92d6
x123 <x123@users.noreply.github.com>
2024-06-10 15:31:21 +0200

    hetznix: enable postgres backups

--- system/hetznix/local/mastodon.nix
+++ system/hetznix/local/mastodon.nix
@@ -1,22 +1,28 @@
 {_, ...}: {
-  services.mastodon = {
-    enable = true;
-    database.createLocally = true;
-    localDomain = "social.boxchop.city";
-    configureNginx = true;
-    smtp.fromAddress = "noreply@social.boxchop.city";
-    extraConfig.SINGLE_USER_MODE = "true";
-    streamingProcesses = 7;
+  services = {
+    mastodon = {
+      enable = true;
+      database.createLocally = true;
+      localDomain = "social.boxchop.city";
+      configureNginx = true;
+      smtp.fromAddress = "noreply@social.boxchop.city";
+      extraConfig.SINGLE_USER_MODE = "true";
+      streamingProcesses = 7;
+    };
+
+    # this is needed to disable automatic ACME cert grab from invidious our own
+    # definition in security.acme.certs (in acme.nix)
+    nginx.virtualHosts = {
+      "social.boxchop.city" = {
+        enableACME = false;
+        useACMEHost = "boxchop.city";
+      };
+    };
+
+    postgresqlBackup = {
+      databases = ["mastodon"];
+    };
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
-
-  # this is needed to disable automatic ACME cert grab from invidious our own
-  # definition in security.acme.certs (in acme.nix)
-  services.nginx.virtualHosts = {
-    "social.boxchop.city" = {
-      enableACME = false;
-      useACMEHost = "boxchop.city";
-    };
-  };
 }
