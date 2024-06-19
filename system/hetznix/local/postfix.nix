95187c0f308ddb8f2fd23365a69d2248fda087c1
x123 <x123@users.noreply.github.com>
2024-06-19 15:21:03 +0200

    add postfix domain

--- system/hetznix/local/postfix.nix
+++ system/hetznix/local/postfix.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   services.postfix = {
     enable = true;
+    domain = "boxchop.city";
     sslCert = config.security.acme.certs."boxchop.city".directory + "/full.pem";
     sslKey = config.security.acme.certs."boxchop.city".directory + "/key.pem";
   };
