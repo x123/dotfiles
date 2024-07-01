632e02d8eb7ac5738051b1082b6955a40018de11
x123 <x123@users.noreply.github.com>
2024-07-01 10:12:43 +0200

    postfix restrictions

--- system/hetznix/local/postfix.nix
+++ system/hetznix/local/postfix.nix
@@ -29,6 +29,23 @@
         smtpd_milters = "unix:/run/opendkim/opendkim.sock";
         non_smtpd_milters = "unix:/run/opendkim/opendkim.sock";
       };
+      extraConfig = ''
+        smtpd_recipient_restrictions =
+          reject_rbl_client zen.spamhaus.org=127.0.0.[2..11]
+          reject_rhsbl_sender dbl.spamhaus.org=127.0.1.[2..99]
+          reject_rhsbl_helo dbl.spamhaus.org=127.0.1.[2..99]
+          reject_rhsbl_reverse_client dbl.spamhaus.org=127.0.1.[2..99]
+          warn_if_reject reject_rbl_client zen.spamhaus.org=127.255.255.[1..255]
+
+        # Reject the request if the sender is the null address and there are multiple recipients
+        smtpd_data_restrictions = reject_multi_recipient_bounce
+
+        # Sender Restrictions
+        smtpd_sender_restrictions =
+          reject_non_fqdn_sender
+          reject_unknown_sender_domain
+          reject_unauthenticated_sender_login_mismatch
+      '';
       sslCert = config.security.acme.certs."boxchop.city".directory + "/full.pem";
       sslKey = config.security.acme.certs."boxchop.city".directory + "/key.pem";
     };
