cc02ae5c39384c10a912c47c3cd9dc56b2f26b76
x123 <x123@users.noreply.github.com>
2024-07-03 14:24:42 +0200

    more smtpd_recipient_restrictions

--- system/hetznix/local/postfix.nix
+++ system/hetznix/local/postfix.nix
@@ -40,6 +40,9 @@
       extraConfig = ''
         smtpd_recipient_restrictions =
           permit_sasl_authenticated
+          reject_non_fqdn_recipient
+          reject_unknown_recipient_domain
+          reject_unauth_destination
           reject_rbl_client zen.spamhaus.org=127.0.0.[2..11]
           reject_rhsbl_sender dbl.spamhaus.org=127.0.1.[2..99]
           reject_rhsbl_helo dbl.spamhaus.org=127.0.1.[2..99]
