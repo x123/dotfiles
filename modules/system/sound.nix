cd79e894960880d52dc0f2c0bc3c39bf23849ca3
x123 <x123@users.noreply.github.com>
2024-07-13 17:27:12 +0200

    disable unnecessary sound.enable

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -1,8 +1,7 @@
-{pkgs, ...}: {
+{...}: {
   imports = [];
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
