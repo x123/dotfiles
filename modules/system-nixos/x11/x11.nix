11dbac654938b16b33f30d6d220bb24cca11ae3d
x123 <x123@users.noreply.github.com>
2024-07-14 18:55:58 +0200

    refactor x11

--- modules/system-nixos/x11/x11.nix
+++ modules/system-nixos/x11/x11.nix
@@ -11,45 +11,47 @@ in {
   config = lib.mkIf (cfg.system-nixos.enable && cfg.system-nixos.x11.enable) {
     programs.dconf.enable = true;
 
-    services.displayManager = {
-      autoLogin.enable = false;
-      defaultSession = "none+i3";
-    };
-
-    services.xserver = {
-      enable = true;
-      xkb = {
-        layout = "us";
-        variant = "";
-      };
-
-      autoRepeatDelay = 250;
-      autoRepeatInterval = 45;
-
-      desktopManager = {
-        xterm.enable = false;
-      };
-
+    services = {
       displayManager = {
-        lightdm = {
-          enable = true;
-          greeter.enable = true;
-        };
+        autoLogin.enable = false;
+        defaultSession = "none+i3";
       };
 
-      windowManager = {
-        i3 = {
-          enable = true;
-          package = pkgs.i3; # or pkgs.i3-gaps
-          extraPackages = builtins.attrValues {
-            inherit
-              (pkgs)
-              dmenu
-              i3status
-              i3lock
-              i3lock-fancy
-              rofi
-              ;
+      xserver = {
+        enable = true;
+        xkb = {
+          layout = "us";
+          variant = "";
+        };
+
+        autoRepeatDelay = 250;
+        autoRepeatInterval = 45;
+
+        desktopManager = {
+          xterm.enable = false;
+        };
+
+        displayManager = {
+          lightdm = {
+            enable = true;
+            greeter.enable = true;
+          };
+        };
+
+        windowManager = {
+          i3 = {
+            enable = true;
+            package = pkgs.i3; # or pkgs.i3-gaps
+            extraPackages = builtins.attrValues {
+              inherit
+                (pkgs)
+                dmenu
+                i3status
+                i3lock
+                i3lock-fancy
+                rofi
+                ;
+            };
           };
         };
       };
