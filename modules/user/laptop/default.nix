97f7c5e018bb1b76e2f62cb0f486e177751bc159
x123 <x123@users.noreply.github.com>
2024-07-14 16:29:38 +0200

    nixpad: screen lock on suspend

--- modules/user/laptop/default.nix
+++ modules/user/laptop/default.nix
@@ -5,9 +5,18 @@
   ...
 }: let
   grep = "${pkgs.gnugrep}/bin/grep";
-  i3lock = "${pkgs.i3lock}/bin/i3lock";
   systemctl = "${pkgs.systemd}/bin/systemctl";
   xset = "${pkgs.xorg.xset}/bin/xset";
+  i3lock-dpms = "${pkgs.writeShellScript "i3lock-dpms" ''
+    set -euo pipefail
+    revert() {
+      ${xset} dpms 0 0 0
+    }
+    trap revert HUP INT TERM
+    ${xset} +dpms dpms 5 5 5
+    ${pkgs.i3lock}/bin/i3lock -n -c 000000
+    revert
+  ''}";
 in {
   imports = [
   ];
@@ -21,28 +30,22 @@ in {
   };
 
   config = lib.mkIf (config.custom.laptop.enable && !pkgs.stdenv.isDarwin) {
+    services.screen-locker = {
+      enable = true;
+      inactiveInterval = 5;
+      lockCmd = i3lock-dpms;
+      xss-lock = {
+        package = pkgs.xss-lock;
+        extraOptions = ["--transfer-sleep-lock"];
+      };
+    };
+
     services.xidlehook = {
       enable = true;
       detect-sleep = true;
       not-when-audio = true;
       not-when-fullscreen = true;
-      environment = {
-        "PRIMARY_DISPLAY" = "eDP-1";
-      };
       timers = [
-        {
-          delay = 300;
-          command = "${pkgs.writeShellScript "i3lock-dpms" ''
-            set -euo pipefail
-            revert() {
-              ${xset} dpms 0 0 0
-            }
-            trap revert HUP INT TERM
-            ${xset} +dpms dpms 5 5 5
-            ${i3lock} -n -c 000000
-            revert
-          ''}";
-        }
         {
           delay = 900;
           command = "${pkgs.writeShellScript "hybrid-sleep" ''
