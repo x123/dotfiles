f7618b83a90963ab039eedad9b6e70485248ced2
x123 <x123@users.noreply.github.com>
2024-07-15 05:32:55 +0200

    nixpad: tlp battery care

--- modules/system-nixos/hardware/laptop.nix
+++ modules/system-nixos/hardware/laptop.nix
@@ -13,6 +13,17 @@ in {
       powertop.enable = true;
     };
 
+    services.tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+
+        START_CHARGE_THRESH_BAT1 = 75;
+        STOP_CHARGE_THRESH_BAT1 = 80;
+      };
+    };
+
     services.logind = {
       lidSwitch = "hybrid-sleep";
       lidSwitchDocked = "ignore";
