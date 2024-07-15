4ff77c69ba55e34b1489511fbd275403e92466dd
x123 <x123@users.noreply.github.com>
2024-07-15 07:44:22 +0200

    hardware.laptop: add thermald

--- modules/system-nixos/hardware/laptop.nix
+++ modules/system-nixos/hardware/laptop.nix
@@ -13,23 +13,29 @@ in {
       powertop.enable = true;
     };
 
-    services.tlp = {
-      enable = true;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 75;
-        STOP_CHARGE_THRESH_BAT0 = 80;
-
-        START_CHARGE_THRESH_BAT1 = 75;
-        STOP_CHARGE_THRESH_BAT1 = 80;
+    services = {
+      logind = {
+        lidSwitch = "hybrid-sleep";
+        lidSwitchDocked = "ignore";
+        lidSwitchExternalPower = "ignore";
+        powerKey = "ignore";
+        powerKeyLongPress = "ignore";
       };
-    };
 
-    services.logind = {
-      lidSwitch = "hybrid-sleep";
-      lidSwitchDocked = "ignore";
-      lidSwitchExternalPower = "ignore";
-      powerKey = "ignore";
-      powerKeyLongPress = "ignore";
+      thermald = {
+        enable = true;
+      };
+
+      tlp = {
+        enable = true;
+        settings = {
+          START_CHARGE_THRESH_BAT0 = 75;
+          STOP_CHARGE_THRESH_BAT0 = 80;
+
+          START_CHARGE_THRESH_BAT1 = 75;
+          STOP_CHARGE_THRESH_BAT1 = 80;
+        };
+      };
     };
 
     systemd.sleep.extraConfig = "HibernateDelaySec=1h";
