90d841f9353e707c9cc15e3ca3ef0e3e109ee61b
x123 <x123@users.noreply.github.com>
2024-07-27 20:42:53 +0200

    poe logout-macro update

--- system/xnix/users/x/local/poe.nix
+++ system/xnix/users/x/local/poe.nix
@@ -2,15 +2,37 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  logout-macro = pkgs.writeShellScript "logout-macro" ''
+    set -euo pipefail
+    sudo ${pkgs.nftables}/bin/nft insert rule inet filter input tcp flags { psh, ack } tcp sport 6112 counter reject with tcp reset
+    HANDLE=$(sudo ${pkgs.nftables}/bin/nft -a list ruleset | ${pkgs.gnugrep}/bin/egrep "tcp sport 6112 counter.*tcp reset # handle ([0-9]+)" | ${pkgs.gawk}/bin/awk -F "# handle " '{print $2}')
+    echo $HANDLE
+    sleep 1
+    sudo ${pkgs.nftables}/bin/nft delete rule inet filter input handle $HANDLE
+  '';
+in {
   home.file = {
     xbindkeysrc = {
       enable = true;
       target = ".xbindkeysrc";
       text = ''
-        "${pkgs.xdotool}/bin/xdotool key --delay 0 --window $( ${pkgs.xdotool}/bin/xdotool search --name Path ) Return slash e x i t Return"
+        "${logout-macro}"
           m:0x0 + c:49
       '';
     };
+    # logout-macro = {
+    #   enable = true;
+    #   executable = true;
+    #   target = "bin/logout-macro";
+    #   source = pkgs.writeShellScript "logout-macro" ''
+    #     set -euo pipefail
+    #     sudo ${pkgs.nftables}/bin/nft insert rule inet filter input tcp flags { psh, ack } tcp sport 6112 counter reject with tcp reset
+    #     HANDLE=$(sudo ${pkgs.nftables}/bin/nft -a list ruleset | ${pkgs.gnugrep}/bin/egrep "tcp sport 6112 counter.*tcp reset # handle ([0-9]+)" | ${pkgs.gawk}/bin/awk -F "# handle " '{print $2}')
+    #     echo $HANDLE
+    #     sleep 1
+    #     sudo ${pkgs.nftables}/bin/nft delete rule inet filter input handle $HANDLE
+    #   '';
+    # };
   };
 }
