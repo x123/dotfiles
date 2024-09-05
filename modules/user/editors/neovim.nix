8219d5f52fbd631746798bcdb27c0468484debab
x123 <x123@users.noreply.github.com>
2024-09-05 21:42:43 +0200

    neovim: oil display options

--- modules/user/editors/neovim.nix
+++ modules/user/editors/neovim.nix
@@ -186,15 +186,14 @@
           skip_confirm_for_simple_edits = true,
           columns = {
             "icon",
-            -- "permissions",
-            -- "size",
-            -- "mtime"
+            "permissions",
           },
           view_options = {
             show_hidden = true,
             natural_order = true,
             is_always_hidden = function(name, _)
-              return name == '..' or name == '.git'
+              return name == '..'
+            --   return name == '..' or name == '.git'
             end,
           },
           win_options = {
