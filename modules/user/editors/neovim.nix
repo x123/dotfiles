bb68aeade049075d5ccf0a1866024febbddacc75
x123 <x123@users.noreply.github.com>
2024-09-06 08:01:29 +0200

    neovim: set space as leader

--- modules/user/editors/neovim.nix
+++ modules/user/editors/neovim.nix
@@ -94,25 +94,26 @@
         vim.opt.background = "dark"
         vim.opt.hlsearch = false
         vim.opt.scrolloff = 10
+        vim.g.mapleader = ' '
 
         -- personal keybinds
-        vim.keymap.set("n", "<space>w\\", "<cmd>vsplit<cr>")
-        vim.keymap.set("n", "<space>ws", "<cmd>split<cr>")
-        vim.keymap.set("n", "<space>qq", "<cmd>quit<cr>")
-        vim.keymap.set("n", "<space>QQ", "<cmd>quit!<cr>")
-        vim.keymap.set("n", "<space>zz", "<cmd>wq<cr>")
-        vim.keymap.set("n", "<space>wx", "<C-W>x")
+        vim.keymap.set("n", "<leader>w\\", "<cmd>vsplit<cr>")
+        vim.keymap.set("n", "<leader>ws", "<cmd>split<cr>")
+        vim.keymap.set("n", "<leader>qq", "<cmd>quit<cr>")
+        vim.keymap.set("n", "<leader>QQ", "<cmd>quit!<cr>")
+        vim.keymap.set("n", "<leader>zz", "<cmd>wq<cr>")
+        vim.keymap.set("n", "<leader>wx", "<C-W>x")
 
-        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
         vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
         vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-        vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+        vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
         -- move visual selection up and down
         vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gvgv")
         vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gvgv")
 
-        vim.keymap.set('n', "<space>U", vim.cmd.UndotreeToggle)
+        vim.keymap.set('n', "<leader>U", vim.cmd.UndotreeToggle)
 
         -- nord
         require("lualine").setup({
@@ -206,10 +207,10 @@
         -- Telescope
         require("telescope").setup()
 
-        vim.keymap.set("n", "<space>ff", "<cmd>Telescope find_files<cr>")
-        vim.keymap.set("n", "<space>fg", "<cmd>Telescope live_grep<cr>")
-        vim.keymap.set("n", "<space>fb", "<cmd>Telescope buffers<cr>")
-        vim.keymap.set("n", "<space>fh", "<cmd>Telescope help_tags<cr>")
+        vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+        vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+        vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+        vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 
         -- tmux navigation
         require("tmux").setup{
@@ -333,16 +334,16 @@
             vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
             vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
             vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-            vim.keymap.set("n", "<space>Wa", vim.lsp.buf.add_workspace_folder, opts)
-            vim.keymap.set("n", "<space>Wr", vim.lsp.buf.remove_workspace_folder, opts)
-            vim.keymap.set("n", "<space>Wl", function()
+            vim.keymap.set("n", "<leader>Wa", vim.lsp.buf.add_workspace_folder, opts)
+            vim.keymap.set("n", "<leader>Wr", vim.lsp.buf.remove_workspace_folder, opts)
+            vim.keymap.set("n", "<leader>Wl", function()
               print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
             end, opts)
-            vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-            vim.keymap.set({"n", "v"}, "<space>ca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, opts)
             vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "<space>F", function()
+            vim.keymap.set("n", "<leader>F", function()
               vim.lsp.buf.format { async = true }
             end, opts)
           end,
