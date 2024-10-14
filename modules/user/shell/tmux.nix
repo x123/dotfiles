f58c431bed6262cbdc84e461882e1dbe61ffd5ec
x123 <x123@users.noreply.github.com>
2024-10-14 16:30:46 +0100

    tmux: remove 3.4 overlay to use 3.5a

--- modules/user/shell/tmux.nix
+++ modules/user/shell/tmux.nix
@@ -1,51 +1,6 @@
 {pkgs, ...}: {
   imports = [];
 
-  # 2024-10-12
-  # tmux in nixpkgs newer than 3.4 breaks default shell in darwin environments
-  # https://github.com/NixOS/nixpkgs/commit/80931ba844da56228943f61b655a061f7f8a06a3
-  nixpkgs.overlays = [
-    (
-      final: prev: {
-        tmux = prev.tmux.overrideAttrs (
-          old: {
-            version = "3.4";
-            src = prev.fetchFromGitHub {
-              owner = "tmux";
-              repo = "tmux";
-              rev = "3.4";
-              version = "3.4";
-              hash = "sha256-RX3RZ0Mcyda7C7im1r4QgUxTnp95nfpGgQ2HRxr0s64=";
-            };
-            patches = [
-              (prev.fetchpatch {
-                url = "https://github.com/tmux/tmux/commit/2d1afa0e62a24aa7c53ce4fb6f1e35e29d01a904.diff";
-                hash = "sha256-mDt5wy570qrUc0clGa3GhZFTKgL0sfnQcWJEJBKAbKs=";
-              })
-              # this patch is designed for android but FreeBSD exhibits the same error for the same reason
-              (prev.fetchpatch {
-                url = "https://github.com/tmux/tmux/commit/4f5a944ae3e8f7a230054b6c0b26f423fa738e71.patch";
-                hash = "sha256-HlUeU5ZicPe7Ya8A1HpunxfVOE2BF6jOHq3ZqTuU5RE=";
-              })
-              # https://github.com/tmux/tmux/issues/3983
-              # fix tmux crashing when neovim is used in a ssh session
-              (prev.fetchpatch {
-                url = "https://github.com/tmux/tmux/commit/aa17f0e0c1c8b3f1d6fc8617613c74f07de66fae.patch";
-                hash = "sha256-jhWGnC9tsGqTTA5tU+i4G3wlwZ7HGz4P0UHl17dVRU4=";
-              })
-              # https://github.com/tmux/tmux/issues/3905
-              # fix tmux hanging on shutdown
-              (prev.fetchpatch {
-                url = "https://github.com/tmux/tmux/commit/3823fa2c577d440649a84af660e4d3b0c095d248.patch";
-                hash = "sha256-FZDy/ZgVdwUAam8g5SfGBSnMhp2nlHHfrO9eJNIhVPo=";
-              })
-            ];
-          }
-        );
-      }
-    )
-  ];
-
   programs.tmux = {
     enable = true;
     historyLimit = 100000;
