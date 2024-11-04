41daa667e2ba9d43974919ba6074ca2da8718d3d
x123 <x123@users.noreply.github.com>
2024-11-04 08:12:10 +0100

    tornet: add nftables nat rules

--- modules/system-nixos/networking/tornet.nix
+++ modules/system-nixos/networking/tornet.nix
@@ -19,6 +19,24 @@ in {
       };
     };
 
-    # networking = {};
+    networking = {
+      #useNetworkd = true;
+      bridges."tornet".interfaces = [];
+      nftables = {
+        enable = true;
+        tables = {
+          nat = {
+            family = "ip";
+            content = ''
+              chain PREROUTING {
+                type nat hook prerouting priority dstnat; policy accept;
+                iifname "tornet" meta l4proto tcp dnat to 127.0.0.1:9040
+                iifname "tornet" udp dport 53 dnat to 127.0.0.1:5353
+              }
+            '';
+          };
+        };
+      };
+    };
   };
 }
