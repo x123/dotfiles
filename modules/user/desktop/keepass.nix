1b488e1b4636cf3a7f9c55972221dd92f555981a
x123 <x123@users.noreply.github.com>
2024-11-08 09:15:04 +0000

    desktop: modular keepassxc

--- modules/user/desktop/keepass.nix
+++ modules/user/desktop/keepass.nix
@@ -6,11 +6,28 @@
 }: {
   imports = [];
 
-  config = lib.mkIf config.custom.desktop.enable {
-    home = {
-      packages = [
-        pkgs.keepassxc
-      ];
+  options = {
+    custom.desktop.keepassxc = {
+      enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = "Whether to enable the keepassxc package.";
+      };
     };
   };
+
+  config =
+    lib.mkIf
+    (
+      config.custom.desktop.enable
+      && config.custom.desktop.keepassxc.enable
+      && !pkgs.stdenv.isDarwin
+    )
+    {
+      home = {
+        packages = [
+          pkgs.keepassxc
+        ];
+      };
+    };
 }
