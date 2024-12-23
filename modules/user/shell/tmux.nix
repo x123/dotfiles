c5487d1f63c823c0ea37027a83ed29f676f7181a
x123 <x123@users.noreply.github.com>
2024-12-23 23:24:37 +0100

    tmux+vim: use vim bindings for tmux-navigator

--- modules/user/shell/tmux.nix
+++ modules/user/shell/tmux.nix
@@ -32,6 +32,11 @@
       # See: https://github.com/christoomey/vim-tmux-navigator
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^.*vim.*$'"
 
+      bind-key -n 'C-h' if-shell "$is_vim" { send-keys C-h }  { select-pane -L }
+      bind-key -n 'C-j' if-shell "$is_vim" { send-keys C-j }  { select-pane -D }
+      bind-key -n 'C-k' if-shell "$is_vim" { send-keys C-k }  { select-pane -U }
+      bind-key -n 'C-l' if-shell "$is_vim" { send-keys C-l }  { select-pane -R }
+
       bind-key -n 'C-Left' if-shell "$is_vim" { send-keys C-Left }  { select-pane -L }
       bind-key -n 'C-Down' if-shell "$is_vim" { send-keys C-Down }  { select-pane -D }
       bind-key -n 'C-Up' if-shell "$is_vim" { send-keys C-Up }  { select-pane -U }
