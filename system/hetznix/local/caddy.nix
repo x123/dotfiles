edca73bef079572faa5902ad25ccddc3b2875cda
x123 <x123@users.noreply.github.com>
2024-12-24 15:57:16 +0100

    hetznix: remove caddy vhost for thumbsup

--- system/hetznix/local/caddy.nix
+++ system/hetznix/local/caddy.nix
@@ -1,12 +1,4 @@
-{config, ...}: {
-  sops.secrets = {
-    "caddy/auth/fam" = {
-      mode = "0440";
-      owner = config.users.users.caddy.name;
-      group = config.users.users.caddy.group;
-    };
-  };
-
+{...}: {
   networking.nftables = {
     tables = {
       filter = {
@@ -29,18 +21,6 @@
       http_port 8880
       https_port 8883
     '';
-    virtualHosts = {
-      "jandf.boxchop.city" = {
-        useACMEHost = "boxchop.city";
-        extraConfig = ''
-          encode zstd gzip
-          root * /var/www/jandf.boxchop.city
-          file_server
-          basic_auth {
-            import ${config.sops.secrets."caddy/auth/fam".path}
-          }
-        '';
-      };
-    };
+    virtualHosts = {};
   };
 }
