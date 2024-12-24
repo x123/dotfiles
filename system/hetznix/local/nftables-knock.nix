d41a7c969e13e127fc510be9f774de91d9b8f1c1
x123 <x123@users.noreply.github.com>
2024-12-24 11:54:26 +0100

    hetznix: update nftables-knock

--- system/hetznix/local/nftables-knock.nix
+++ system/hetznix/local/nftables-knock.nix
@@ -7,82 +7,154 @@
         filter = {
           family = "inet";
           content = ''
-            set ks1 {
+            set ipv6_ks1 {
+              type ipv6_addr
+              flags timeout
+              timeout 10s
+              gc-interval 4s
+            }
+            set ipv6_ks2 {
+              type ipv6_addr
+              flags timeout
+              timeout 10s
+              gc-interval 4s
+            }
+            set ipv6_ks3 {
+              type ipv6_addr
+              flags timeout
+              timeout 10s
+              gc-interval 4s
+            }
+            set ipv6_ks4 {
+              type ipv6_addr
+              flags timeout
+              timeout 60m
+              gc-interval 4s
+            }
+
+            chain ipv6_k1 {
+              set add ip6 saddr @ipv6_ks1
+            }
+            chain ipv6_uk1 {
+              set update ip6 saddr timeout 0s @ipv6_ks1
+            }
+            chain ipv6_k2 {
+              set update ip6 saddr timeout 0s @ipv6_ks1
+              set add ip6 saddr @ipv6_ks2
+            }
+            chain ipv6_uk2 {
+              set update ip6 saddr timeout 0s @ipv6_ks2
+            }
+            chain ipv6_k3 {
+              set update ip6 saddr timeout 0s @ipv6_ks2
+              set add ip6 saddr @ipv6_ks3
+            }
+            chain ipv6_uk3 {
+              set update ip6 saddr timeout 0s @ipv6_ks3
+            }
+            chain ipv6_k4 {
+              set update ip6 saddr timeout 0s @ipv6_ks3
+              set add ip6 saddr @ipv6_ks4 log prefix "nft-pn accepted: " level info
+            }
+
+            chain ipv6_refk {
+              set update ip6 saddr timeout 60m @ipv6_ks4
+            }
+
+            # 8209, 15388, 48665, 49676
+            chain ipv6_pk {
+              ct state new ip6 saddr @ipv6_ks4 goto ipv6_refk
+              tcp dport 49676 ct state new ip6 saddr @ipv6_ks3 goto ipv6_k4
+              tcp dport 48665 ct state new ip6 saddr @ipv6_ks3 return
+              ip6 saddr @ipv6_ks3 ct state new goto ipv6_uk3
+              tcp dport 48665 ct state new ip6 saddr @ipv6_ks2 goto ipv6_k3
+              tcp dport 15388 ct state new ip6 saddr @ipv6_ks2 return
+              ip6 saddr @ipv6_ks2 ct state new goto ipv6_uk2
+              tcp dport 15388 ct state new ip6 saddr @ipv6_ks1 goto ipv6_k2
+              tcp dport 8209 ct state new ip6 saddr @ipv6_ks1 return
+              ip6 saddr @ipv6_ks1 ct state new goto ipv6_uk1
+              tcp dport 8209 ct state new goto ipv6_k1
+            }
+
+            set ipv4_ks1 {
               type ipv4_addr
               flags timeout
               timeout 10s
               gc-interval 4s
             }
-            set ks2 {
+            set ipv4_ks2 {
               type ipv4_addr
               flags timeout
               timeout 10s
               gc-interval 4s
             }
-            set ks3 {
+            set ipv4_ks3 {
               type ipv4_addr
               flags timeout
               timeout 10s
               gc-interval 4s
             }
-            set ks4 {
+            set ipv4_ks4 {
               type ipv4_addr
               flags timeout
               timeout 60m
               gc-interval 4s
             }
 
-            chain k1 {
-              set add ip saddr @ks1
+            chain ipv4_k1 {
+              set add ip saddr @ipv4_ks1
             }
-            chain uk1 {
-              set update ip saddr timeout 0s @ks1
+            chain ipv4_uk1 {
+              set update ip saddr timeout 0s @ipv4_ks1
             }
-            chain k2 {
-              set update ip saddr timeout 0s @ks1
-              set add ip saddr @ks2
+            chain ipv4_k2 {
+              set update ip saddr timeout 0s @ipv4_ks1
+              set add ip saddr @ipv4_ks2
             }
-            chain uk2 {
-              set update ip saddr timeout 0s @ks2
+            chain ipv4_uk2 {
+              set update ip saddr timeout 0s @ipv4_ks2
             }
-            chain k3 {
-              set update ip saddr timeout 0s @ks2
-              set add ip saddr @ks3
+            chain ipv4_k3 {
+              set update ip saddr timeout 0s @ipv4_ks2
+              set add ip saddr @ipv4_ks3
             }
-            chain uk3 {
-              set update ip saddr timeout 0s @ks3
+            chain ipv4_uk3 {
+              set update ip saddr timeout 0s @ipv4_ks3
             }
-            chain k4 {
-              set update ip saddr timeout 0s @ks3
-              set add ip saddr @ks4 log prefix "nft-pn accepted: " level info
+            chain ipv4_k4 {
+              set update ip saddr timeout 0s @ipv4_ks3
+              set add ip saddr @ipv4_ks4 log prefix "nft-pn accepted: " level info
             }
 
-            chain refk {
-              set update ip saddr timeout 60m @ks4
+            chain ipv4_refk {
+              set update ip saddr timeout 60m @ipv4_ks4
             }
 
             # 8209, 15388, 48665, 49676
-            chain pk {
-              ct state new ip saddr @ks4 goto refk
-              tcp dport 49676 ct state new ip saddr @ks3 goto k4
-              tcp dport 48665 ct state new ip saddr @ks3 return
-              ip saddr @ks3 ct state new goto uk3
-              tcp dport 48665 ct state new ip saddr @ks2 goto k3
-              tcp dport 15388 ct state new ip saddr @ks2 return
-              ip saddr @ks2 ct state new goto uk2
-              tcp dport 15388 ct state new ip saddr @ks1 goto k2
-              tcp dport 8209 ct state new ip saddr @ks1 return
-              ip saddr @ks1 ct state new goto uk1
-              tcp dport 8209 ct state new goto k1
+            chain ipv4_pk {
+              ct state new ip saddr @ipv4_ks4 goto ipv4_refk
+              tcp dport 49676 ct state new ip saddr @ipv4_ks3 goto ipv4_k4
+              tcp dport 48665 ct state new ip saddr @ipv4_ks3 return
+              ip saddr @ipv4_ks3 ct state new goto ipv4_uk3
+              tcp dport 48665 ct state new ip saddr @ipv4_ks2 goto ipv4_k3
+              tcp dport 15388 ct state new ip saddr @ipv4_ks2 return
+              ip saddr @ipv4_ks2 ct state new goto ipv4_uk2
+              tcp dport 15388 ct state new ip saddr @ipv4_ks1 goto ipv4_k2
+              tcp dport 8209 ct state new ip saddr @ipv4_ks1 return
+              ip saddr @ipv4_ks1 ct state new goto ipv4_uk1
+              tcp dport 8209 ct state new goto ipv4_k1
             }
 
             chain input-new {
               # pk
-              jump pk
+              jump ipv4_pk
+              jump ipv6_pk
 
               # ssh
-              tcp dport 22 ip saddr @ks4 log prefix "nft-input-pn-traffic-accept: " level info
-              tcp dport 22 ip saddr @ks4 counter accept
+              tcp dport 22 ip saddr @ipv4_ks4 log prefix "nft-input-pn-traffic-accept: " level info
+              tcp dport 22 ip saddr @ipv4_ks4 counter accept
+              tcp dport 22 ip6 saddr @ipv6_ks4 log prefix "nft-input-pn-traffic-accept: " level info
+              tcp dport 22 ip6 saddr @ipv6_ks4 counter accept
             }
           '';
         };
