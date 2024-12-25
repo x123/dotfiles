ab16300db9e31a98b76674522d43fe7fcf50d57d
x123 <x123@users.noreply.github.com>
2024-12-25 10:04:02 +0100

    hetznix: add ip6 caddy vhost

--- system/hetznix/local/caddy.nix
+++ system/hetznix/local/caddy.nix
@@ -31,6 +31,16 @@
           encode zstd gzip
         '';
       };
+      "ip6.boxchop.city" = {
+        listenAddresses = ["2a01:4f8:1c1b:51d1::6"];
+        useACMEHost = "boxchop.city";
+        extraConfig = ''
+          templates
+          header Content-Type text/plain
+          respond "{{.RemoteIP}}"
+          encode zstd gzip
+        '';
+      };
     };
   };
 }
