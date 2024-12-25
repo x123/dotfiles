e4affd7402916b9c736e36d131e7c7c4ab053715
x123 <x123@users.noreply.github.com>
2024-12-25 10:16:24 +0100

    hetznix: disable blockblast nginx vhost

--- system/hetznix/local/nginx.nix
+++ system/hetznix/local/nginx.nix
@@ -1,14 +1,14 @@
 {_, ...}: {
   # this is needed to disable automatic ACME cert grab from invidious our own
   # definition in security.acme.certs (in acme.nix)
-  services.nginx.virtualHosts = {
-    "blockblaster.boxchop.city" = {
-      enableACME = false;
-      useACMEHost = "boxchop.city";
-      forceSSL = true;
-      root = "/var/www/blockblaster.boxchop.city";
-      locations."/blockblaster" = {
-      };
-    };
-  };
+  # services.nginx.virtualHosts = {
+  #   "blockblaster.boxchop.city" = {
+  #     enableACME = false;
+  #     useACMEHost = "boxchop.city";
+  #     forceSSL = true;
+  #     root = "/var/www/blockblaster.boxchop.city";
+  #     locations."/blockblaster" = {
+  #     };
+  #   };
+  # };
 }
