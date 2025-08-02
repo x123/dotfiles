f1df6ad477d9cf00ca7535379b2b779680e12756
x123 <x123@users.noreply.github.com>
2025-08-02 11:26:21 +0200

    neovim: bigfile limit 2mb->10mb

--- modules/user/editors/neovim.nix
+++ modules/user/editors/neovim.nix
@@ -160,7 +160,7 @@ in {
 
         -- bigfile
         require("bigfile").setup({
-          filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+          filesize = 10, -- size of the file in MiB, the plugin round file sizes to the closest MiB
           pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
           features = { -- features to disable
             "indent_blankline",
