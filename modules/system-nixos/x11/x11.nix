3561f6e6c4bdad7c7d1c310734490624f4625271
x123 <x123@users.noreply.github.com>
2025-08-20 21:24:56 +0200

    system-nixos/x11: enable xdg

--- modules/system-nixos/x11/x11.nix
+++ modules/system-nixos/x11/x11.nix
@@ -10,6 +10,29 @@
     programs.dconf.enable = true;
     programs.i3lock.enable = true;
 
+    security.rtkit.enable = true;
+
+    xdg.autostart.enable = true;
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal
+        #xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gtk
+      ];
+      config = {
+        common = {
+          default = ["gtk"];
+          "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
+        };
+        hyprland = {
+          default = ["hyprland" "gtk"];
+          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+          "org.freedesktop.impl.portal.OpenURI" = ["gtk"];
+        };
+      };
+    };
+
     services = {
       displayManager = {
         autoLogin.enable = false;
