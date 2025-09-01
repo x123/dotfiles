74382c401b51f355cfc59d298fb54cb06dc85729
x123 <x123@users.noreply.github.com>
2025-09-01 10:19:55 +0200

    Reapply "shell/ssh: options migration"
    This reverts commit 8cf660f95bd3c192327dcc539bd2c810cc6ac25c.

--- modules/user/shell/ssh.nix
+++ modules/user/shell/ssh.nix
@@ -17,12 +17,13 @@
   config = lib.mkIf (config.custom.user.shell.enable && config.custom.user.shell.ssh.enable) {
     programs.ssh = {
       enable = true;
-      compression = true;
-      forwardAgent = false;
-      # instead of yes give 52 weeks see Time Formats in man 5 sshd_config
-      addKeysToAgent = "52w";
+      enableDefaultConfig = false;
       matchBlocks = {
         "*" = {
+          # instead of yes give 52 weeks see Time Formats in man 5 sshd_config
+          addKeysToAgent = "52w";
+          compression = true;
+          forwardAgent = false;
           serverAliveInterval = 60;
           extraOptions = {
             ConnectTimeout = "10";
