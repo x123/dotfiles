a1a0430ffdc2a59da13f3b7d3d10a4def45b60f7
x123 <x123@users.noreply.github.com>
2025-09-10 08:53:43 +0200

    jellyfin: modular firewall

--- modules/system-nixos/services/jellyfin.nix
+++ modules/system-nixos/services/jellyfin.nix
@@ -2,15 +2,31 @@
   config,
   lib,
   ...
-}: {
-  imports = [];
-
+}: let
+  trustedIpv4s = builtins.concatStringsSep "," config.custom.system-nixos.services.jellyfin.trustedIpv4Networks;
+  trustedIpv6s = builtins.concatStringsSep "," config.custom.system-nixos.services.jellyfin.trustedIpv6Networks;
+in {
   options = {
     custom.system-nixos.services.jellyfin = {
       enable = lib.mkOption {
         default = false;
         type = lib.types.bool;
         description = "Whether to enable the system jellyfin service";
+      };
+      openFirewallNftables = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = "Whether to add nftables rules.";
+      };
+      trustedIpv4Networks = lib.mkOption {
+        default = ["127.0.0.1/32"];
+        type = lib.types.listOf lib.types.str;
+        description = "Trusted IPv4 ranges to open nftables firewall for.";
+      };
+      trustedIpv6Networks = lib.mkOption {
+        default = ["::1/128"];
+        type = lib.types.listOf lib.types.str;
+        description = "Trusted IPv6 ranges to open nftables firewall for.";
       };
     };
   };
@@ -19,6 +35,30 @@
     services.jellyfin = {
       enable = true;
       openFirewall = true;
+    };
+
+    networking.nftables = lib.mkIf (config.custom.system-nixos.services.jellyfin.openFirewallNftables) {
+      tables = {
+        filter = {
+          family = "inet";
+          content = ''
+            chain input-new {
+              # jellyfin
+              ip6 saddr { ${trustedIpv6s} } tcp dport { 8096, 8920 } log prefix "nft-input-accept-jellyfin: " level info
+              ip6 saddr { ${trustedIpv6s} } tcp dport { 8096, 8920 } counter accept
+
+              ip6 saddr { ${trustedIpv6s} } udp dport { 7359 } log prefix "nft-input-accept-jellyfin-discovery: " level info
+              ip6 saddr { ${trustedIpv6s} } udp dport { 7359 } counter accept
+
+              ip saddr { ${trustedIpv4s} } tcp dport { 8096, 8920 } log prefix "nft-input-accept-jellyfin: " level info
+              ip saddr { ${trustedIpv4s} } tcp dport { 8096, 8920 } counter accept
+
+              ip saddr { ${trustedIpv4s} } udp dport { 7359 } log prefix "nft-input-accept-jellyfin-discovery: " level info
+              ip saddr { ${trustedIpv4s} } udp dport { 7359 } counter accept
+            }
+          '';
+        };
+      };
     };
   };
 }
--- system/xnix/configuration.nix
+++ system/xnix/configuration.nix
@@ -83,7 +83,19 @@
         ];
       };
 
-      jellyfin.enable = true;
+      jellyfin = {
+        enable = true;
+        openFirewallNftables = true;
+        trustedIpv4Networks = [
+          "192.168.1.0/24"
+          "192.168.9.0/24"
+        ];
+        trustedIpv6Networks = [
+          "fdab:817c:904c::1/60" # gk-2
+          "fd65:4e21:dde4::/60" # gk
+        ];
+      };
+
       nftables.enable = true;
       nix-ssh-serve.enable = false;
       qdrant.enable = true;
