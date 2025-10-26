2af6e9be2cceff96d54b5b2e77dda7724d3650f9
x123@users.noreply.github.com <x123>
2025-10-26 20:50:44 +0100

    mininix disko updates

--- system/mininix/disk-config.nix
+++ system/mininix/disk-config.nix
@@ -7,19 +7,20 @@
         content = {
           type = "gpt";
           partitions = {
-            # boot = {
-            #   name = "boot";
-            #   size = "1M";
-            #   type = "EF02";
-            # };
             boot = {
               name = "boot";
+              size = "4M";
+              type = "EF02";
+            };
+            ESP = {
+              name = "ESP";
               size = "500M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
               };
             };
             # plainSwap = {
