c0fb645694fab95b8d455cdebcc5b9c0d2c2c490
x123@users.noreply.github.com <x123>
2025-10-26 21:51:30 +0100

    hetznix: add thelounge

---
+++ system/hetznix/local/thelounge.nix
@@ -0,0 +1,26 @@
+{pkgs, ...}: {
+  services = {
+    thelounge = {
+      enable = true;
+      port = 9000;
+      public = false;
+      extraConfig = {
+        reverseProxy = true;
+        host = "127.0.0.1";
+      };
+    };
+
+    caddy = {
+      enable = true;
+      virtualHosts = {
+        "tl.nixlink.net" = {
+          useACMEHost = "nixlink.net";
+          extraConfig = ''
+            reverse_proxy 127.0.0.1:9000
+            redir @httpget https://{host}{uri}
+          '';
+        };
+      };
+    };
+  };
+}
