af880f37cb46fc59fb0f4e32277fdb0516648671
x123@users.noreply.github.com <x123>
2025-10-27 13:43:10 +0100

    i2p fixups

--- system/privnix/local/i2p.nix
+++ system/privnix/local/i2p.nix
@@ -2,20 +2,30 @@
   services.i2pd = {
     enable = true;
     bandwidth = 256;
+    address = "192.168.1.151";
+    proto = {
+      http.enable = true;
+      socksProxy.enable = true;
+      httpProxy.enable = true;
+      sam.enable = true;
+    };
   };
-  # networking = {
-  #   nftables = {
-  #     enable = true; # enable nftables
-  #     tables = {
-  #       filter = {
-  #         family = "inet";
-  #         content = ''
-  #           chain input-new {
-  #             tcp dport 2062 counter accept comment "Allow TCP/2062"
-  #           }
-  #         '';
-  #       };
-  #     };
-  #   };
-  # };
+  networking = {
+    nftables = {
+      enable = true; # enable nftables
+      tables = {
+        filter = {
+          family = "inet";
+          content = ''
+            chain input-new {
+              tcp dport 7656 counter accept comment "Allow i2pd.sam/7656"
+              tcp dport 7070 counter accept comment "Allow i2pd.web/7070"
+              tcp dport 4447 counter accept comment "Allow i2pd.socksproxy/4447"
+              tcp dport 4444 counter accept comment "Allow i2pd.httpproxy/4444"
+            }
+          '';
+        };
+      };
+    };
+  };
 }
