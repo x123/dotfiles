6b6d33a264da71f974a8c3ffac8f76a00e9d9a98
x123@users.noreply.github.com <x123>
2025-10-28 13:20:33 +0100

    hetznix: add iodine

---
+++ system/hetznix/local/iodine.nix
@@ -0,0 +1,35 @@
+{config, ...}: {
+  sops.secrets = {
+    "iodinePass" = {
+      mode = "0440";
+      owner = config.users.users.iodined.name;
+      group = config.users.users.iodined.group;
+    };
+  };
+
+  services = {
+    iodine.server = {
+      enable = true;
+      domain = "t.nixlink.net";
+      ip = "10.42.0.1/24";
+      passwordFile = config.sops.secrets."iodinePass".path;
+    };
+  };
+
+  networking = {
+    nftables = {
+      enable = true; # enable nftables
+      tables = {
+        filter = {
+          family = "inet";
+          content = ''
+            chain input-new {
+              # iodine/DNS
+              udp dport 53 counter accept comment "Allow UDP/53"
+            }
+          '';
+        };
+      };
+    };
+  };
+}
