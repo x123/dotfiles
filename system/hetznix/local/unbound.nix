8f6040f178dc0116b4bbb653727491f0a1216fcb
x123@users.noreply.github.com <x123>
2025-10-29 09:24:17 +0100

    hetznix: unbound dns over tls only

--- system/hetznix/local/unbound.nix
+++ system/hetznix/local/unbound.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
-  # users.users.postfix.extraGroups = ["opendkim"];
+  users.users.unbound.extraGroups = ["acme"];
 
   services = {
     unbound = {
@@ -8,7 +8,12 @@
       checkconf = true; # wont work if settings.remote-control or settings.include are used
       settings = {
         server = {
-          interface = ["0.0.0.0" "::0"];
+          interface = [
+            # "0.0.0.0@53"
+            # "::0@53"
+            "0.0.0.0@853"
+            "::0@853"
+          ];
           access-control = ["0.0.0.0/0 allow" "::0/0 allow"];
           harden-glue = true;
           harden-dnssec-stripped = true;
@@ -17,6 +22,8 @@
           edns-buffer-size = 1232;
           hide-identity = true;
           hide-version = true;
+          tls-service-pem = config.security.acme.certs."nixlink.net".directory + "/cert.pem";
+          tls-service-key = config.security.acme.certs."nixlink.net".directory + "/key.pem";
         };
       };
     };
@@ -31,8 +38,8 @@
           content = ''
             chain input-new {
               # unbound
-              udp dport 53 counter accept comment "Allow UDP/53 unbound"
-              tcp dport 53 counter accept comment "Allow TCP/53 unbound"
+              # udp dport 53 counter accept comment "Allow UDP/53 unbound"
+              # tcp dport 53 counter accept comment "Allow TCP/53 unbound"
               tcp dport 853 counter accept comment "Allow TCP/853 unbound DNS-over-TLS"
             }
           '';
