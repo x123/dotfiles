67ce7cc08889e61423de47c3c7123976331cc92b
x123 <x123@users.noreply.github.com>
2025-11-25 06:12:26 +0100

    neovim lua consistency

--- modules/user/neovim.nix
+++ modules/user/neovim.nix
@@ -68,19 +68,19 @@
       vim.keymap.set("n", "<space>zz", "<cmd>wq<cr>")
       vim.keymap.set("n", "<space>wx", "<C-W>x")
 
-      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
       -- move visual selection up and down
       vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
       vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv")
 
       -- nord
-      require('lualine').setup({
+      require("lualine").setup({
         options = {
-          theme = 'nord'
+          theme = "nord"
         }
       })
 
@@ -129,7 +129,7 @@
       require("mason").setup()
       require("mason-lspconfig").setup()
 
-      require("nvim-treesitter.configs").setup {
+      require("nvim-treesitter.configs").setup({
         indent = {
           enable = true,
         },
@@ -139,54 +139,54 @@
         highlight = {
           enable = true,
         },
-      }
+      })
 
-      require("nvim-lastplace").setup {
+      require("nvim-lastplace").setup({
         lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
         lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
         lastplace_open_folds = true
-      }
+      })
 
       local lspconfig = require("lspconfig")
-      lspconfig.cssls.setup {}
-      lspconfig.elixirls.setup {
+      lspconfig.cssls.setup({})
+      lspconfig.elixirls.setup({
         cmd = { "${pkgs.elixir-ls}/bin/elixir-ls" };
-      }
-      lspconfig.eslint.setup {}
-      lspconfig.gopls.setup {}
-      lspconfig.html.setup {}
-      lspconfig.jsonls.setup {}
-      lspconfig.lua_ls.setup {}
-      lspconfig.marksman.setup {}
-      lspconfig.nixd.setup {}
-      lspconfig.terraformls.setup {}
+      })
+      lspconfig.eslint.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.marksman.setup({})
+      lspconfig.nixd.setup({})
+      lspconfig.terraformls.setup({})
 
       -- Use LspAttach autocommand to only map the following keys
       -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
           -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
           local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<space>Wa', vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set('n', '<space>Wr', vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set('n', '<space>Wl', function()
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<space>Wa", vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set("n", "<space>Wr", vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set("n", "<space>Wl", function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           end, opts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<space>F', function()
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set({"n", "v"}, "<space>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<space>F", function()
             vim.lsp.buf.format { async = true }
           end, opts)
         end,
