4546142d124e07ee3e9098fb7f6c5e64b6acadf5
x123 <x123@users.noreply.github.com>
2025-11-25 06:10:40 +0100

    try flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "x123 system config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
+  outputs = { nixpkgs, home-manager, ... }:
+  let 
+    system = "x86_64-linux";
+    
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };  
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+    nixosConfigurations = {
+      xnix = lib.nixosSystem {
+        inherit system;
+        
+        modules = [
+          ./system/configuration.nix
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+            home-manager.users.x = import ./users/x/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];  
+      };      
+    };
+  };
+}
