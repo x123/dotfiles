8dae54209716547410ba9e7d0e7e54b67098f5b4
x123 <x123@users.noreply.github.com>
2025-11-25 06:16:37 +0100

    user/dev/llm: disable aider-chat

--- modules/user/dev/llm.nix
+++ modules/user/dev/llm.nix
@@ -8,11 +8,11 @@
     custom.user.dev.llm.enable = lib.mkEnableOption "LLM and AI tools" // {default = true;};
   };
 
+  # TODO add aider-chat-full back in once it's not broken
   config = lib.mkIf (config.custom.user.dev.enable && config.custom.user.dev.llm.enable) {
     home.packages = builtins.attrValues {
       inherit
         (pkgs.unstable-small)
-        aider-chat-full
         claude-code
         fabric-ai
         opencode
