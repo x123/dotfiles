a817e11f90a204a49025eabdf8a152c280158400
x123 <x123@users.noreply.github.com>
2025-11-25 06:17:12 +0100

    mininix: convert tftp to system-nixos module

--- system/mininix/configuration.nix
+++ system/mininix/configuration.nix
@@ -9,15 +9,25 @@
     ./disk-config.nix
     ../../modules/nix-settings # do not remove
     ../../modules/system-nixos
-
-    # ./local/thelounge.nix
-    ./local/tftpd.nix
   ];
 
   custom.system-nixos = {
     enable = true;
 
     services = {
+      tftp = {
+        enable = true;
+        trustedIpv4Networks = [
+          "192.168.1.0/24"
+          "192.168.9.0/24"
+        ];
+        trustedIpv6Networks = [
+          "fdab:817c:904c::1/60" # gk-2
+          "fd65:4e21:dde4::1/60" # gk
+        ];
+        root = "/srv/tftp";
+      };
+
       openssh = {
         enable = true;
         openFirewallNftables = true;
