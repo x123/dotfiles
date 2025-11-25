ab0da8dc65b90b6974d8014f1f3b472e78e01d9e
x123 <x123@users.noreply.github.com>
2025-11-25 06:13:24 +0100

    update nvidia options

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -5,22 +5,22 @@
 }: {
   imports = [];
 
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = [pkgs.vaapiVdpau];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = [pkgs.vaapiVdpau];
+    };
+    nvidia = {
+      modesetting.enable = true;
+      forceFullCompositionPipeline = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
 
   services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    forceFullCompositionPipeline = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
 }
