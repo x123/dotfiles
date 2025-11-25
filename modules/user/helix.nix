42a90e9377062610e52c9d05c41f6289fb51934b
x123 <x123@users.noreply.github.com>
2025-11-25 06:12:23 +0100

    add helix editor

---
+++ modules/user/helix.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }: {
+  imports = [ ];
+
+  home = {
+    packages = with pkgs; [
+      helix
+    ];
+  };
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = false;
+    settings = {
+      theme = "nord";
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+      };
+    };
+  };
+  
+}
