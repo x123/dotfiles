f6a7c284372beec6200a4af14d8d1956a83c037d
x123 <x123@users.noreply.github.com>
2025-11-25 06:15:03 +0100

    fom-mbp: add system

---
+++ system/fom-mbp/users/fom/home.nix
@@ -0,0 +1,106 @@
+{lib, ...}: {
+  imports = [
+    ../../../../modules/user
+  ];
+
+  custom = {
+    desktop = {
+      enable = true;
+
+      blender.enable = false;
+      gimp.enable = true;
+      video.enable = true;
+    };
+
+    editors = {
+      helix.enable = false;
+      neovim.enable = true;
+      vim.enable = false;
+    };
+
+    games.enable = true;
+    mail.enable = false;
+  };
+
+  home = {
+    username = "fom";
+    homeDirectory = "/Users/fom";
+    stateVersion = "23.05";
+    sessionVariables = {
+      TESTINGVAR = "testing";
+    };
+
+    shellAliases = {
+      "todo!" = "cd ~/work/src/todo; sops todo.md";
+    };
+  };
+
+  programs = {
+    # force override the font size for alacritty
+    alacritty = {
+      settings = {
+        font = {
+          size = lib.mkForce 16;
+        };
+      };
+    };
+
+    home-manager.enable = true;
+
+    ssh = {
+      includes = [
+        "config.d/*"
+      ];
+      matchBlocks = {
+        "github.com" = {
+          hostname = "github.com";
+          port = 22;
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-work";
+          identitiesOnly = true;
+        };
+        "me.github.com" = {
+          hostname = "github.com";
+          port = 22;
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-priv";
+          identitiesOnly = true;
+        };
+        "work.github.com" = {
+          hostname = "github.com";
+          port = 22;
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-work";
+          identitiesOnly = true;
+        };
+        "hetznix" = {
+          hostname = "hetznix.boxchop.city";
+          port = 22;
+          user = "root";
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-priv";
+          identitiesOnly = true;
+        };
+        "hetznix.boxchop.city" = {
+          hostname = "hetznix.boxchop.city";
+          port = 22;
+          user = "root";
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-priv";
+          identitiesOnly = true;
+        };
+        "xnix.lan" = {
+          hostname = "xnix.lan";
+          port = 22;
+          addressFamily = "inet";
+          user = "x";
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-priv";
+          identitiesOnly = true;
+        };
+        "nixpad.lan" = {
+          hostname = "nixpad.lan";
+          port = 22;
+          addressFamily = "inet";
+          user = "x";
+          identityFile = "/Users/fom/.ssh/id_fom-mbp-priv";
+          identitiesOnly = true;
+        };
+      };
+    };
+  };
+}
