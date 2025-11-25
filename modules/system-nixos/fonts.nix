700b43ee195c7bef665142b55ea4e231dc643ccc
x123 <x123@users.noreply.github.com>
2025-11-25 06:16:32 +0100

    system-nixos: fonts fix

--- modules/system-nixos/fonts.nix
+++ modules/system-nixos/fonts.nix
@@ -8,7 +8,15 @@
 in {
   imports = [];
 
-  config = lib.mkIf (cfg.system-darwin.enable && cfg.system-darwin.fonts.enable) {
+  options = {
+    custom.system-nixos.fonts.enable = lib.mkOption {
+      default = true;
+      type = lib.types.bool;
+      description = "Whether to enable fonts on nixos";
+    };
+  };
+
+  config = lib.mkIf (cfg.system-nixos.enable && cfg.system-nixos.fonts.enable) {
     fonts.packages = [
       pkgs.nerd-fonts.code-new-roman
       pkgs.powerline-fonts
