d232cd9682b79e5ef6d94f307ba3377c37a8c0e2
x123 <x123@users.noreply.github.com>
2025-11-25 06:14:51 +0100

    tornet: enable firejail

--- modules/system-nixos/networking/tornet.nix
+++ modules/system-nixos/networking/tornet.nix
@@ -9,6 +9,24 @@ in {
   imports = [];
 
   config = lib.mkIf (cfg.system-nixos.enable && cfg.system-nixos.networking.tornet.enable) {
+    programs.firejail = {
+      enable = true;
+      # wrappedBinaries = {
+      #   librewolf = {
+      #     executable = "${pkgs.librewolf}/bin/librewolf";
+      #     profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
+      #     extraArgs = [
+      #       # Required for U2F USB stick
+      #       # "--ignore=private-dev"
+      #       # Enforce dark mode
+      #       "--env=GTK_THEME=Adwaita:dark"
+      #       # Enable system notifications
+      #       "--dbus-user.talk=org.freedesktop.Notifications"
+      #     ];
+      #   };
+      # };
+    };
+
     services.tor = {
       enable = true;
       openFirewall = true;
