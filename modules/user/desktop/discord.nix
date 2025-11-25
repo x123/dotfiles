5b71fcea2b6e2ef580d9fe663608bf308f3278ea
x123 <x123@users.noreply.github.com>
2025-11-25 06:13:39 +0100

    remove discord overlay

--- modules/user/desktop/discord.nix
+++ modules/user/desktop/discord.nix
@@ -15,20 +15,20 @@
       && config.custom.desktop.discord.enable
     )
     {
-      nixpkgs.overlays = [
-        (
-          final: prev: {
-            discord = prev.discord.overrideAttrs (
-              _: {
-                src = builtins.fetchTarball {
-                  url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-                  sha256 = "1xjk77g9lj5b78c1w3fj42by9b483pkbfb41yzxrg4p36mnd2hkn";
-                };
-              }
-            );
-          }
-        )
-      ];
+      # nixpkgs.overlays = [
+      #   (
+      #     final: prev: {
+      #       discord = prev.discord.overrideAttrs (
+      #         _: {
+      #           src = builtins.fetchTarball {
+      #             url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+      #             sha256 = "sha256:1xjk77g9lj5b78c1w3fj42by9b483pkbfb41yzxrg4p36mnd2hkn";
+      #           };
+      #         }
+      #       );
+      #     }
+      #   )
+      # ];
 
       home = {
         packages = [
