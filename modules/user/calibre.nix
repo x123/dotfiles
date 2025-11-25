616e36c52e3f04fb6bde018a73487ebc0085df60
x123 <x123@users.noreply.github.com>
2025-11-25 06:11:32 +0100

    comment calibre override

--- modules/user/calibre.nix
+++ modules/user/calibre.nix
@@ -1,6 +1,21 @@
 {pkgs, ...}: {
   imports = [];
 
+  # calibre
+#  nixpkgs.overlays = [(
+#    self: super: {
+#      calibre = super.calibre.overrideAttrs (
+#        _: {
+#          version = "7.2.0";
+#          src = builtins.fetchurl {
+#            url = "https://download.calibre-ebook.com/7.2.0/calibre-7.2.0.tar.xz";
+#            sha256 = "1OZPSXF5cQlmwbD2bHVWtYHLUgCo8LaR1WPpuSUWoR8=";
+#        }; }
+#      );
+#    }
+#    )
+#  ];
+
   home = {
     packages = with pkgs; [
       calibre
