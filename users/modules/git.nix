f0953a44516038842679c4f0e4ea3632e722c76b
x123 <x123@users.noreply.github.com>
2025-11-25 06:10:52 +0100

    git push.autoSetupRemote = true

--- users/modules/git.nix
+++ users/modules/git.nix
@@ -5,6 +5,7 @@
     enable = true;
     userName = "x123";
     userEmail = "x123@users.noreply.github.com";
-  };
+    extraConfig.push.autoSetupRemote = true; #push.autoSetupRemote true
 
+  };
 }
