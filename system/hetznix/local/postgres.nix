fc5fe0b81f92bad830b3db04cb0a835965c1de5c
x123 <x123@users.noreply.github.com>
2025-11-25 06:13:44 +0100

    hetznix: adjust local postgres auth

--- system/hetznix/local/postgres.nix
+++ system/hetznix/local/postgres.nix
@@ -1,5 +1,20 @@
-{_, ...}: {
+{pkgs, ...}: {
   services.postgresqlBackup = {
     enable = true;
+  };
+
+  services.postgresql = {
+    enable = true;
+    identMap = ''
+      # ArbitraryMapName systemUser DBUser
+         superuser_map      root      postgres
+         superuser_map      postgres  postgres
+         # Let other names login as themselves
+         superuser_map      /^(.*)$   \1
+    '';
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method optional_ident_map
+      local sameuser  all     peer        map=superuser_map
+    '';
   };
 }
