bec064fbb76fc02cf9d76bf44300eb386e14bacf
x123 <x123@users.noreply.github.com>
2025-11-25 06:12:17 +0100

    simplify starship prompt

--- modules/user/shell/starship.nix
+++ modules/user/shell/starship.nix
@@ -8,7 +8,7 @@
     settings = {
       add_newline = false;
 
-      format = "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
+      format = "$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
 
       gcloud.disabled = false;
 
