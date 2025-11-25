598964bd3f3d0affd0179fdd599a3465bea769ca
x123 <x123@users.noreply.github.com>
2025-11-25 06:11:33 +0100

    switch to direnv

--- shells/elixir/standard/shell.nix
+++ shells/elixir/standard/shell.nix
@@ -4,11 +4,12 @@ with pkgs;
 
 let
   inherit (lib) optional optionals;
+  #elixir = beam.packages.erlangR24.elixir_1_14;
   elixir = beam.packages.erlangR25.elixir_1_15;
 in
 
 mkShell {
-  buildInputs = [ elixir git ]
+  buildInputs = [ elixir git erlang ]
     ++ optional stdenv.isLinux libnotify # For ExUnit Notifier on Linux.
     ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
     ++ optional stdenv.isDarwin terminal-notifier # For ExUnit Notifier on macOS.
