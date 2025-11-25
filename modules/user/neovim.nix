c6942a0fa30231864056af43ac2a50b5c780625b
x123 <x123@users.noreply.github.com>
2025-11-25 06:11:52 +0100

    setup nvim-lastplace

--- modules/user/neovim.nix
+++ modules/user/neovim.nix
@@ -100,6 +100,12 @@
         },
       }
 
+      require("nvim-lastplace").setup {
+        lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+        lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+        lastplace_open_folds = true
+      }
+
       local lspconfig = require("lspconfig")
       lspconfig.cssls.setup {}
       lspconfig.elixirls.setup {
