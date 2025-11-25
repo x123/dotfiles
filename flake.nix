d9a685e700111975d91d866119a73a96fcebe27b
x123 <x123@users.noreply.github.com>
2025-11-25 06:17:26 +0100

    unboundednix: add lxc based container

--- flake.nix
+++ flake.nix
@@ -366,6 +366,29 @@
         ];
       };
 
+      unboundednix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+          system = "x86_64-linux";
+          hostname = "unboundednix.empire.internal";
+        };
+        modules = [
+          ./system/unboundednix/configuration.nix
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                inherit inputs;
+              };
+            };
+          }
+        ];
+      };
+
       vm = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         specialArgs = {
