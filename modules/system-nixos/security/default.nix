242952cdcb722511c6afcbf0db8f68c0cf03747d
x123 <x123@users.noreply.github.com>
2025-11-25 06:14:59 +0100

    security: fix duplicate auditd options

--- modules/system-nixos/security/default.nix
+++ modules/system-nixos/security/default.nix
@@ -4,14 +4,4 @@
     ./auditd.nix
     ./firejail.nix
   ];
-
-  options = {
-    custom.system-nixos.security = {
-      auditd.enable = lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = "Whether to enable system auditd";
-      };
-    };
-  };
 }
