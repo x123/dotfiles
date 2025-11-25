bd5c1c0e6e3ad39f257e44ea6cd48bd1d9756501
x123 <x123@users.noreply.github.com>
2025-11-25 06:11:59 +0100

    trusted substituters and cleanup time

--- system/xnix/configuration.nix
+++ system/xnix/configuration.nix
@@ -23,9 +23,6 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.secrets.test_secret = {};
 
-  nix.settings.trusted-substituters = ["https://ai.cachix.org"];
-  nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
-
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
