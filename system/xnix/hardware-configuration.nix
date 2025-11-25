bbcb602c69e9830f8a9c1da06525e120ac7f9ecf
x123 <x123@users.noreply.github.com>
2025-11-25 06:15:41 +0100

    Revert "xnix: pin kernel 6.12"
    This reverts commit c2d40411c14b9f127abadcab420873cb12bd2e52.

--- system/xnix/hardware-configuration.nix
+++ system/xnix/hardware-configuration.nix
@@ -23,9 +23,8 @@
     initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" "usb_storage"];
     initrd.kernelModules = [];
     kernelParams = [];
-    # FIXME 2025-01-23 6.13 nvidia iis broken, see https://github.com/NixOS/nixpkgs/issues/375730
-    kernelPackages = pkgs.linuxKernel.packages.linux_6_12;
-    # kernelPackages = pkgs.linuxPackages_latest; # lqx or zen or latest
+    # kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
+    kernelPackages = pkgs.linuxPackages_latest; # lqx or zen or latest
     kernelModules = [
       "kvm-amd"
       "nct6683"
