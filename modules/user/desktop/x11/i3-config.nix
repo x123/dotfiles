3b108103d83a0139612148bd7807049f3ab4519a
x123 <x123@users.noreply.github.com>
2025-11-25 06:15:36 +0100

    i3: autohide status bar

--- modules/user/desktop/x11/i3-config.nix
+++ modules/user/desktop/x11/i3-config.nix
@@ -268,8 +268,8 @@ in {
                 names = ["CodeNewRoman Nerd Font Mono"];
                 size = 11.0;
               };
-              mode = "dock";
-              hiddenState = "show";
+              mode = "hide";
+              hiddenState = "hide";
               statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
               position = "bottom";
               trayOutput = "primary";
