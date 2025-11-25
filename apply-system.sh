9dbf994a8b3786a77e55ac5b0b628819974da671
x123 <x123@users.noreply.github.com>
2025-11-25 06:10:55 +0100

    add darwin config

--- apply-system.sh
+++ apply-system.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-
-pushd ~/.dotfiles
-sudo nixos-rebuild switch --flake .#
-popd
+set -x
+DARWIN_REBUILD=`which darwin-rebuild`
+NIXOS_REBUILD=`which nixos-rebuild`
+if [ ! -z "${DARWIN_REBUILD}" ]; then
+	echo "Found darwin-rebuild at ${DARWIN_REBUILD}"
+	pushd ~/.dotfiles
+	darwin-rebuild switch --flake .#
+	popd
+elif [ ! -z "${NIXOS_REBUILD}" ]; then
+	echo "Found nixos-rebuild at ${NIXOS_REBUILD}"
+	pushd ~/.dotfiles
+	sudo nixos-rebuild switch --flake .#
+	popd;
+else
+	echo "Could not find nixos-rebuild or darwin-rebuild ... Bailing out!"
+	exit 1
+fi
