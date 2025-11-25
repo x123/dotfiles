2121d0c9d11d67a3a44876f4153876062e9d9dbc
x123 <x123@users.noreply.github.com>
2025-11-25 06:15:00 +0100

    firejail: Discord

--- modules/user/desktop/discord.nix
+++ modules/user/desktop/discord.nix
@@ -31,6 +31,31 @@
       # ];
 
       home = {
+        file = {
+          firejail-Discord-profile = {
+            enable = true;
+            text = ''
+              include ${pkgs.firejail}/etc/firejail/Discord.profile
+              blacklist ${pkgs.nixVersions.stable}/bin/*
+              noblacklist ''${HOME}/.nix-profile/share/themes
+              # blacklist ''${HOME}/.nix-profile/bin/*
+              blacklist ''${HOME}/.nix-profile/etc/*
+              blacklist ''${HOME}/.nix-profile/example/*
+              blacklist ''${HOME}/.nix-profile/include/*
+              # blacklist ''${HOME}/.nix-profile/lib/*
+              blacklist ''${HOME}/.nix-profile/libexec/*
+              blacklist ''${HOME}/.nix-profile/manifest.nix
+              # blacklist ''${HOME}/.nix-profile/opt/*
+              blacklist ''${HOME}/.nix-profile/rplugin.vim
+              blacklist ''${HOME}/.nix-profile/sbin/*
+              blacklist ''${HOME}/.nix-profile/share/*
+              blacklist ''${HOME}/.nix-profile/XyGrib/*
+              blacklist ''${HOME}/.nix-profile/yed/*
+            '';
+            target = ".config/firejail/Discord.profile";
+          };
+        };
+
         packages = [
           pkgs.discord
         ];
