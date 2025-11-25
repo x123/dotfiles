b24af1a710941b51854e0d409e5d2dc23dc7b664
x123 <x123@users.noreply.github.com>
2025-11-25 06:14:58 +0100

    security: improve auditd conf

--- modules/system-nixos/security/auditd.nix
+++ modules/system-nixos/security/auditd.nix
@@ -7,6 +7,16 @@
 in {
   imports = [];
 
+  options = {
+    custom.system-nixos.security.auditd = {
+      enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = "Whether to enable auditd";
+      };
+    };
+  };
+
   config = lib.mkIf (cfg.system-nixos.enable && cfg.system-nixos.security.auditd.enable) {
     security = {
       auditd.enable = true;
