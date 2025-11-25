33152b57ec10e548fb5ff4c2f9f16dfbb8dab334
x123 <x123@users.noreply.github.com>
2025-11-25 06:10:41 +0100

    add discord

--- users/x/home.nix
+++ users/x/home.nix
@@ -10,6 +10,17 @@
   };
 
   imports = [];
+  nixpkgs.overlays = [(
+    self: super: {
+      discord = super.discord.overrideAttrs (
+        _: { src = builtins.fetchTarball {
+          url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+          sha256 = "1xjk77g9lj5b78c1w3fj42by9b483pkbfb41yzxrg4p36mnd2hkn";
+        }; }
+      );
+    }
+    )
+  ];
 
   nixpkgs = {
     config = {
@@ -19,6 +30,7 @@
 
   home.packages = with pkgs; [
     alacritty
+    discord
     dropbox
     firefox
     git
