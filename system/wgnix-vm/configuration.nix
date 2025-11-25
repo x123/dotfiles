c764c7e3027a18bbdaa0f243f48a7ea0ee9f018f
x123 <x123@users.noreply.github.com>
2025-11-25 06:17:27 +0100

    resolver 192.168.1.127->192.168.1.165

--- system/wgnix-vm/configuration.nix
+++ system/wgnix-vm/configuration.nix
@@ -83,7 +83,7 @@
         replace=domain/${config.networking.domain}/
       '';
     };
-    nameservers = ["192.168.1.127"];
+    nameservers = ["192.168.1.165"];
     search = [];
   };
 
@@ -93,7 +93,7 @@
       matchConfig.Name = "enp6s18";
       networkConfig = {
         DNS = [
-          "192.168.1.127"
+          "192.168.1.165"
         ];
         MulticastDNS = false;
         LLMNR = false;
@@ -225,7 +225,7 @@
   # systemd-resolved disalbed for now (favoring old-school resolvconf)
   services.resolved = {
     enable = false; # commenting this out will cause resolvconf to fail
-    fallbackDns = ["192.168.1.127"];
+    fallbackDns = ["192.168.1.165"];
     llmnr = "false";
   };
 
