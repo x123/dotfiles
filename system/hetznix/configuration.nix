396efc220b4b9127f1adeaaee5bf604c0db05343
x123 <x123@users.noreply.github.com>
2025-11-25 06:15:04 +0100

    hetznix: add key

--- system/hetznix/configuration.nix
+++ system/hetznix/configuration.nix
@@ -105,6 +105,7 @@
     users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAV4W4TVF5yqOwKFax+b2XtRYbdKy1wy4zFXfFZfv5be xnix"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6elYl8CWSR32Zx33D+XgQWM/721sDmnyFJec7vDeMb fom-mba"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMXxyU8SafRkTGJ3ZGMlsl9iXu7Yb+IT2R2OHg0KVx+E fom-mbp"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGXOucJH+GXgiD/ro01zTxFOquY5g3oE6FULjV59Sgz nixpad"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKkPCWWFGc0PYrwwpzpykM20aXZxLLd+hD8/W4ga7nAA x@vm"
     ];
