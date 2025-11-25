6db5f3721f4816e145277d90f66a63956955fd10
x123 <x123@users.noreply.github.com>
2025-11-25 06:11:46 +0100

    add sops to nixium

--- system/nixium/configuration.nix
+++ system/nixium/configuration.nix
@@ -9,6 +9,12 @@
 
   boot.kernelPackages = pkgs.linuxPackages_zen; # lqx or zen or latest
 
+  sops.defaultSopsFile = ./secrets.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.age.generateKey = true;
+  sops.secrets."ssh/nixium/private" = {};
+  sops.secrets."ssh/nixium/public" = {};
+
   networking = {
     hostName = "nixium";
     domain = "boxchop.city";
