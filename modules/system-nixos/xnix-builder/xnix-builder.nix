aab3cc86cf07d6189a471866611a26a972695a52
x123 <x123@users.noreply.github.com>
2025-11-25 06:17:25 +0100

    xnix-builder: fixups

--- modules/system-nixos/xnix-builder/xnix-builder.nix
+++ modules/system-nixos/xnix-builder/xnix-builder.nix
@@ -6,6 +6,21 @@
   imports = [];
 
   config = lib.mkIf (config.custom.system-nixos.enable && config.custom.system-nixos.xnix-builder.enable) {
+    programs.ssh = {
+      knownHosts = {
+        xnix = {
+          hostNames = ["xnix" "192.168.1.131" "xnix.empire.internal"];
+          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4gsyVG0ah/z4zDUJt0/a0l/JaWN6VIa7QBlDcOk4Sp";
+        };
+      };
+      extraConfig = ''
+        Host xnix.empire.internal
+          User builder
+          IdentityFile /etc/ssh/ssh_host_ed25519_key
+          IdentitiesOnly yes
+      '';
+    };
+
     nix = {
       distributedBuilds = true;
 
@@ -13,7 +28,7 @@
         connect-timeout = 5;
         builders-use-substitutes = true;
         # max-jobs = 0; # to disable building locally
-        #extra-substituters = ["ssh://xnix.empire.internal"];
+        extra-substituters = ["ssh://builder@xnix.empire.internal"];
         fallback = true;
       };
 
