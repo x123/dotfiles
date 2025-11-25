01b8abf5f3176add3f1eb1b13d3b730c82c0ef23
x123 <x123@users.noreply.github.com>
2025-11-25 06:16:04 +0100

    nix download-buffer-size

--- modules/nix-settings/default.nix
+++ modules/nix-settings/default.nix
@@ -29,6 +29,7 @@
 
     optimise.automatic = true;
     settings = {
+      download-buffer-size = 524288000;
       experimental-features = ["nix-command" "flakes"];
       allowed-users = ["*"];
       trusted-users = [
