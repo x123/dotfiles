f3ec6b1649bf96035c306ec613ac3b48bffc2785
x123 <x123@users.noreply.github.com>
2025-11-25 06:15:52 +0100

    nixpad: add unstable-small

--- system/nixpad/users/x/home.nix
+++ system/nixpad/users/x/home.nix
@@ -9,6 +9,16 @@
     # ./local/borgmatic.nix
   ];
 
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        unstable-small = import inputs.nixpkgs-unstable-small {
+          system = "x86_64-linux";
+        };
+      }
+    )
+  ];
+
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     defaultSopsFile = ./secrets.yaml;
