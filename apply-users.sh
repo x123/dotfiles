47def9370a3892ee37ba1abf77676e117f1e1ae0
x123 <x123@users.noreply.github.com>
2025-11-25 06:10:55 +0100

    darwin preparation

--- apply-users.sh
+++ apply-users.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -x
 USER=`whoami`
+HOSTNAME=`hostname -s`
 if [ -z "${USER}" ]
 then
-	echo "Not able to set USER with whoami. Bailing";
+	echo "Not able to set USER with whoami ... Bailing out";
+	exit 1;
+fi
+if [ -z "${HOSTNAME}" ]
+then
+	echo "Not able to get HOSTNAME with hostname -s ... Bailing out";
 	exit 1;
 fi
 
@@ -12,7 +18,7 @@ then
 	echo "Current USER is ${USER}. This is meant to be run at a user-level (not root)."
 	exit 1;
 fi
-echo $USER
+echo "${USER}-${HOSTNAME}"
 pushd ~/.dotfiles
-nix build .#homeManagerConfigurations.${USER}.activationPackage
+nix build .#homeManagerConfigurations.${USER}-${HOSTNAME}.activationPackage
 ./result/activate
--- flake.nix
+++ flake.nix
@@ -4,33 +4,35 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nur.url = "github:nix-community/NUR";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nur.url = "github:nix-community/NUR";
+
+    # minimize duplicate instances of inputs
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
   
   outputs = { nixpkgs, home-manager, nur, nixos-wsl, ... }:
   let 
-    #system = "x86_64-linux";
-    
-    pkgs = import nixpkgs {
-      #inherit system;
-      config = { allowUnfree = true; };  
-    };
-
     lib = nixpkgs.lib;
 
   in {
     homeManagerConfigurations = {
-      nixos = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [
-        ./users/nixos/home.nix
+      nixos-xnixwsl = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
+        modules = [
+          ./users/nixos/home.nix
       ];
     };
 
-      x = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      x-xnix = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
         modules = [
           nur.nixosModules.nur
           ./users/x/home.nix
