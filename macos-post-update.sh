d80403d92859397029c69de39ab4207c5961f3ae
x123 <x123@users.noreply.github.com>
2025-11-25 06:11:23 +0100

    fix macos script

--- macos-post-update.sh
+++ macos-post-update.sh
@@ -5,4 +5,4 @@ sudo mv /etc/bashrc /etc/bashrc.orig.${DATESTAMP}
 sudo mv /etc/zshrc /etc/zshrc.orig.${DATESTAMP}
 sudo mv /etc/zprofile /etc/zprofile.orig.${DATESTAMP}
 sudo mv /etc/shells /etc/shells.orig.${DATESTAMP}
-sudo /nix/var/nix/profiles/system/activate.${DATESTAMP}
+sudo /nix/var/nix/profiles/system/activate
