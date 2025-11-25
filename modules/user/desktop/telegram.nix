7dfee6cc7c9759e6e4843e7ec3191a5c1f861d42
x123 <x123@users.noreply.github.com>
2025-11-25 06:14:15 +0100

    desktop: make telegram optional

--- modules/user/desktop/telegram.nix
+++ modules/user/desktop/telegram.nix
@@ -6,10 +6,21 @@
 }: {
   imports = [];
 
+  options = {
+    custom.desktop.telegram = {
+      enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = "Whether to enable the telegram package.";
+      };
+    };
+  };
+
   config =
     lib.mkIf
     (
       config.custom.desktop.enable
+      && config.custom.desktop.telegram.enable
       && !pkgs.stdenv.isDarwin
     )
     {
