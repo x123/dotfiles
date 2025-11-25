3b6db780de18078c5609303fe86933b7ec29dfac
x123 <x123@users.noreply.github.com>
2025-11-25 06:13:22 +0100

    postfix mvp

--- system/hetznix/local/postfix.nix
+++ system/hetznix/local/postfix.nix
@@ -1,7 +1,17 @@
 {config, ...}: {
   services.postfix = {
     enable = true;
+    enableHeaderChecks = true;
     domain = "boxchop.city";
+    config = {
+      myhostname = "hetznix.boxchop.city";
+      myorigin = "boxchop.city";
+      relayhost = "";
+      inet_protocols = "ipv4";
+      inet_interfaces = "loopback-only";
+      mydestination = "boxchop.city";
+      home_mailbox = "Maildir/";
+    };
     sslCert = config.security.acme.certs."boxchop.city".directory + "/full.pem";
     sslKey = config.security.acme.certs."boxchop.city".directory + "/key.pem";
   };
