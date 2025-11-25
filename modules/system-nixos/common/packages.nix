21a47d9fd4a7d6fc14640b2b2fcadf9356ab7fb9
x123 <x123@users.noreply.github.com>
2025-11-25 06:17:23 +0100

    system-nixos/common: add common packages

---
+++ modules/system-nixos/common/packages.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [];
+  options = {
+    custom.system-nixos.common.packages.enable = lib.mkOption {
+      default = true;
+      type = lib.types.bool;
+      description = "Whether to enable common packages";
+    };
+  };
+
+  config = lib.mkIf (config.custom.system-nixos.enable && config.custom.system-nixos.common.packages.enable) {
+    environment.systemPackages = [
+      pkgs.dnsutils
+      pkgs.iftop
+      pkgs.sysstat
+      pkgs.tcpdump
+    ];
+  };
+}
