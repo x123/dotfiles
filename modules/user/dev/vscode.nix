6135102efcc8b550df72e9815301d1c15dfcf00d
x123 <x123@users.noreply.github.com>
2025-11-25 06:16:15 +0100

    vscode changes

--- modules/user/dev/vscode.nix
+++ modules/user/dev/vscode.nix
@@ -1,30 +1,106 @@
 {
   config,
   inputs,
+  lib,
   pkgs,
   ...
 }: {
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscodium;
-    profiles.default.extensions = builtins.attrValues {
-      inherit
-        (pkgs.vscode-extensions.rooveterinaryinc)
-        roo-cline
-        ;
-      inherit
-        (pkgs.vscode-extensions.vscodevim)
-        vim
-        ;
-      inherit
-        (pkgs.vscode-extensions.github)
-        copilot
-        copilot-chat
-        ;
-      inherit
-        (pkgs.vscode-extensions.davidanson)
-        vscode-markdownlint
-        ;
-    };
-  };
+  config =
+    lib.mkMerge
+    [
+      (lib.mkIf
+        (pkgs.stdenv.isLinux)
+        {
+          programs.vscode = {
+            enable = true;
+            package = pkgs.vscodium;
+            profiles.default.extensions = builtins.attrValues {
+              inherit
+                (pkgs.vscode-extensions.rooveterinaryinc)
+                roo-cline
+                ;
+              inherit
+                (pkgs.vscode-extensions.vscodevim)
+                vim
+                ;
+              inherit
+                (pkgs.vscode-extensions.github)
+                copilot
+                copilot-chat
+                ;
+              inherit
+                (pkgs.vscode-extensions.davidanson)
+                vscode-markdownlint
+                ;
+            };
+          };
+          # home.packages = builtins.attrValues {
+          #   inherit
+          #     (inputs.nixpkgs-unstable-small.legacyPackages.${system})
+          #     aider-chat-full
+          #     ;
+          #   inherit
+          #     (pkgs)
+          #     claude-code
+          #     ;
+          # };
+        })
+      (lib.mkIf
+        (pkgs.stdenv.isDarwin)
+        {
+          programs.vscode = {
+            enable = true;
+            package = pkgs.vscode;
+            profiles.default.extensions = builtins.attrValues {
+              inherit
+                (pkgs.vscode-extensions.rooveterinaryinc)
+                roo-cline
+                ;
+              inherit
+                (pkgs.vscode-extensions.vscodevim)
+                vim
+                ;
+              inherit
+                (pkgs.vscode-extensions.github)
+                copilot
+                copilot-chat
+                ;
+              inherit
+                (pkgs.vscode-extensions.davidanson)
+                vscode-markdownlint
+                ;
+            };
+          };
+          # home.packages = builtins.attrValues {
+          #   inherit
+          #     (inputs.nixpkgs-unstable-small.legacyPackages.${system})
+          #     aider-chat
+          #     ;
+          # };
+        })
+    ];
+
+  # programs.vscode = {
+  #   enable = true;
+  #   package = pkgs.vscodium;
+  #   profiles.default.extensions = builtins.attrValues {
+  #     inherit
+  #       (pkgs.vscode-extensions.rooveterinaryinc)
+  #       roo-cline
+  #       ;
+  #     inherit
+  #       (pkgs.vscode-extensions.vscodevim)
+  #       vim
+  #       ;
+  #     inherit
+  #       (pkgs.vscode-extensions.github)
+  #       copilot
+  #       copilot-chat
+  #       ;
+  #     inherit
+  #       (pkgs.vscode-extensions.davidanson)
+  #       vscode-markdownlint
+  #       ;
+  #   };
+  # };
 }
