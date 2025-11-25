c71899e648dbffd44e52342978ecc261099b201e
x123 <x123@users.noreply.github.com>
2025-11-25 06:17:21 +0100

    xnix: syncthing fix

--- system/xnix/users/x/local/syncthing.nix
+++ system/xnix/users/x/local/syncthing.nix
@@ -8,5 +8,22 @@
     guiAddress = "127.0.0.1:8384";
     settings.gui.user = "x";
     tray.enable = true;
+    # overrideDevices = false;
+    # overrideFolders = false;
+    settings = {
+      devices = {
+        ios = {
+          id = "HRM4WDT-MEG5OOT-S4EEEEG-NCZ3H2F-574WNEB-CZFJ546-AKLVSWW-EBA7MAB";
+          autoAcceptFolders = true;
+          name = "ios";
+        };
+      };
+      folders = {
+        "/home/x/syncthing/keepass-sync" = {
+          id = "keepass-sync";
+          devices = ["ios"];
+        };
+      };
+    };
   };
 }
