c303d08a1eb34716b35bf8585574d294d5abd5c4
x123 <x123@users.noreply.github.com>
2025-11-25 06:13:38 +0100

    nvd diffs on home-manager switch

--- modules/user/common/home-manager.nix
+++ modules/user/common/home-manager.nix
@@ -1,5 +1,14 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [];
+
+  # report nvd diffs after home-manager switch
+  home.activation.report-changes = config.lib.dag.entryAnywhere ''
+    ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff $oldGenPath $newGenPath
+  '';
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
