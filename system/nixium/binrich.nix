9b180dc835d3c2715a9bcc23ab82924a4a632cd4
x123 <x123@users.noreply.github.com>
2025-11-25 06:12:30 +0100

    restart binrich on fails

--- system/nixium/binrich.nix
+++ system/nixium/binrich.nix
@@ -16,6 +16,7 @@
     description = "binrich";
     shell = pkgs.zsh;
     useDefaultShell = true;
+    linger = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAV4W4TVF5yqOwKFax+b2XtRYbdKy1wy4zFXfFZfv5be xnix"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6elYl8CWSR32Zx33D+XgQWM/721sDmnyFJec7vDeMb fom-mba"
