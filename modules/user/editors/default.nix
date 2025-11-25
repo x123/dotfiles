a19751f85d63d6ea37058b4529d998f221a0e2e7
x123 <x123@users.noreply.github.com>
2025-11-25 06:16:29 +0100

    user/editors: cleanup

--- modules/user/editors/default.nix
+++ modules/user/editors/default.nix
@@ -1,8 +1,18 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   imports = [
     ./emacs.nix
     ./helix.nix
     ./neovim.nix
     ./vim.nix
   ];
+
+  options.custom.user.editors.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Enable editor configurations";
+  };
 }
