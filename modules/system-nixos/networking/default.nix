d61c613c4dbe108a723ce58a61e116f82076bbbb
x123 <x123@users.noreply.github.com>
2025-11-25 06:14:59 +0100

    networking: fix duplicate tornet options

--- modules/system-nixos/networking/default.nix
+++ modules/system-nixos/networking/default.nix
@@ -2,14 +2,4 @@
   imports = [
     ./tornet.nix
   ];
-
-  options = {
-    custom.system-nixos.networking = {
-      tornet.enable = lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = "Whether to enable system tornet network bridge";
-      };
-    };
-  };
 }
